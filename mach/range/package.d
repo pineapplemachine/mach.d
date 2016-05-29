feadb32d25947d052d127575809756ff90d1026b
Sophie Kirschner <pineapplemachine@gmail.com>
2016-05-29 12:17:00 -0400

    Added a package.d to mach.range

---
+++ mach/range/package.d
@@ -0,0 +1,16 @@
+module mach.range;
+
+import mach.range.asarray : asarray;
+import mach.range.asrange : asrange;
+import mach.range.compare : compare, equals;
+import mach.range.contains : contains, containsrange, containselement;
+import mach.range.each : each;
+import mach.range.enumerate : enumerate;
+import mach.range.filter : filter;
+import mach.range.indexof : indexof, indexofrange, indexofelement;
+import mach.range.logical : any, all, none, count, exactly, more, less, atleast, atmost;
+import mach.range.map : map;
+import mach.range.pluck : pluck;
+import mach.range.reduce : reduce;
+import mach.range.reduction : min, max, sum, product;
+import mach.range.reversed : reversed;
