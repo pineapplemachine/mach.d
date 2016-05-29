38dfb96cc33a0d29ade8aa1a7b02091191dd5bb3
Sophie Kirschner <pineapplemachine@gmail.com>
2016-05-29 11:04:49 -0400

    Added mach.range.map

---
+++ mach/range/map.d
@@ -0,0 +1,69 @@
+module mach.range.map;
+
+private:
+
+import mach.traits : isRange;
+import mach.range.asrange : asrange, validAsRange;
+import mach.range.metarange : MetaRangeMixin;
+
+public:
+
+
+
+alias canMap = validAsRange;
+alias canMapRange = isRange;
+
+
+
+/// Returns a range whose elements are those of the given iterable transformed
+/// by some function.
+auto map(alias transform, Iter)(Iter iter) if(canMap!Iter){
+    auto range = iter.asrange;
+    return MapRange!(transform, typeof(range))(range);
+}
+
+
+
+struct MapRange(alias transform, Range) if(canMapRange!Range){
+    mixin MetaRangeMixin!(
+        Range, `source`, `RandomAccess Slice`,
+        `return transform(this.source.front);`,
+        `this.source.popFront();`
+    );
+    
+    Range source;
+    
+    this(typeof(this) range){
+        this(range.source);
+    }
+    this(Range source){
+        this.source = source;
+    }
+    
+    static if(isRandomAccessRange!Range){
+        auto opIndex(IndexParameters!Range index){
+            return transform(this.source.opIndex(index));
+        }
+    }
+    
+    // TODO: Slice
+}
+
+
+
+version(unittest){
+    private:
+    import mach.error.unit;
+    import mach.range.compare : equals;
+}
+unittest{
+    tests("Map", {
+        int[] ones = [1, 1, 1, 1];
+        int[] empty = new int[0];
+        alias square = (n) => (n * n);
+        test([1, 2, 3, 4].map!square.equals([1, 4, 9, 16]));
+        test(ones.map!square.equals(ones));
+        test("Empty input", empty.map!square.equals(empty));
+        testeq("Random access", [2, 3].map!square[1], 9);
+    });
+}
