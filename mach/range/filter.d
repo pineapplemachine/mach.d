dcb88b912ecdd1344c78db01dc6709d25d412a0b
Sophie Kirschner <pineapplemachine@gmail.com>
2016-06-01 19:22:49 -0400

    Updated mach.range.filter to allow mutability

--- mach/range/filter.d
+++ mach/range/filter.d
@@ -2,7 +2,8 @@ module mach.range.filter;
 
 private:
 
-import std.range.primitives : isForwardRange, isBidirectionalRange;
+import mach.traits : isSavingRange, isBidirectionalRange;
+import mach.traits : isMutableRange, isMutableFrontRange, isMutableBackRange;
 import mach.range.asrange : asrange, validAsRange;
 import mach.range.meta : MetaRangeMixin;
 
@@ -20,15 +21,10 @@ auto filter(alias pred, Iter)(Iter iter) if(validAsRange!Iter){
 
 
 struct FilterRange(alias pred, Range){
+    alias Element = typeof(Range.front);
+    
     mixin MetaRangeMixin!(
-        Range, `source`,
-        `Empty Save Back`,
-        `
-            return this.source.front;
-        `, `
-            this.source.popFront();
-            this.consumeFront();
-        `
+        Range, `source`, `Empty Save Back`
     );
     
     Range source;
@@ -39,6 +35,13 @@ struct FilterRange(alias pred, Range){
         static if(isBidirectionalRange!Range) this.consumeBack();
     }
     
+    @property auto ref front(){
+        return this.source.front;
+    }
+    void popFront(){
+        this.source.popFront();
+        this.consumeFront();
+    }
     /// Pop values from source range until a matching value is found.
     void consumeFront(){
         while(!this.source.empty && !pred(this.source.front)){
@@ -47,11 +50,34 @@ struct FilterRange(alias pred, Range){
     }
     
     static if(isBidirectionalRange!Range){
+        @property auto ref back(){
+            return this.source.back;
+        }
+        void popBack(){
+            this.source.popBack();
+            this.consumeBack();
+        }
         void consumeBack(){
             while(!this.source.empty && !pred(this.source.back)){
                 this.source.popBack();
             }
         }
+    }
+    
+    static if(isMutableRange!Range){
+        enum bool mutable = true;
+        static if(isMutableFrontRange!Range){
+            @property void front(Element value){
+                this.source.front = value;
+            }
+        }
+        static if(isMutableBackRange!Range){
+            @property void back(Element value){
+                this.source.back = value;
+            }
+        }
+    }else{
+        enum bool mutable = false;
     }
 }
 
@@ -61,9 +87,23 @@ version(unittest){
     private:
     import mach.error.unit;
     import mach.range.compare : equals;
+    import mach.range.consume : consume;
+    import mach.range.mutate : mutate;
+    import mach.range.reversed : reversed;
 }
 unittest{
     tests("Filter", {
-        test([1, 2, 3, 4, 5, 6].filter!((n) => (n % 2 == 0)).equals([2, 4, 6]));
+        alias even = (n) => (n % 2 == 0);
+        tests("Iteration", {
+            test([1, 2, 3, 4, 5, 6].filter!even.equals([2, 4, 6]));
+        });
+        tests("Backwards", {
+            test([2, 4, 5].filter!even.reversed.equals([4, 2]));
+        });
+        tests("Mutability", {
+            auto array = [1, 2, 3, 4, 5, 6];
+            array.filter!even.mutate!((n) => (n - 1)).consume;
+            testeq(array, [1, 1, 3, 3, 5, 5]);
+        });
     });
 }
