9337f18db41986be33c9c578e131eabfb73b49b9
Sophie Kirschner <pineapplemachine@gmail.com>
2016-06-01 18:10:47 -0400

    Minor code cleanup in mach.range.each

--- mach/range/each.d
+++ mach/range/each.d
@@ -6,6 +6,8 @@ import mach.traits : isFiniteIterable, isFiniteIterableReverse;
 
 public:
 
+
+
 void each(alias func, Iter)(Iter iter) if(isFiniteIterable!Iter){
     foreach(item; iter){
         func(item);
@@ -18,18 +20,20 @@ void eachreverse(alias func, Iter)(Iter iter) if(isFiniteIterableReverse!Iter){
     }
 }
 
+
+
 version(unittest){
     import mach.error.unit;
     import mach.range.compare : equals;
 }
 unittest{
-    auto input = [1, 2, 3, 4, 5];
+    int[] input = [1, 2, 3, 4, 5];
     // Forward
     int[] output;
     input.each!((i){output ~= i;});
     test(output.equals(input));
     // Reverse
-    int[] output_reverse;
-    input.eachreverse!((i){output_reverse ~= i;});
-    test(output_reverse.equals([5, 4, 3, 2, 1]));
+    int[] outputreverse;
+    input.eachreverse!((i){outputreverse ~= i;});
+    test(outputreverse.equals([5, 4, 3, 2, 1]));
 }
