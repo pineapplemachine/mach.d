aab5ae9cbf3448bc62839eb245d86b206baf8ac4
Sophie Kirschner <pineapplemachine@gmail.com>
2016-06-02 15:28:19 -0400

    Better-defined constraints for mach.range.reduce

--- mach/range/reduce.d
+++ mach/range/reduce.d
@@ -10,9 +10,23 @@ public:
 
 alias canReduce = isFiniteIterable;
 
+enum canReduce(Iter, alias func) = canReduce!(Iter, ElementType!Iter, func);
+
+template canReduce(Iter, Acc, alias func){
+    enum bool canReduce = is(typeof((inout int = 0){
+        alias Element = ElementType!Iter;
+        auto element = Element.init;
+        auto first = func(Acc.init, element);
+        auto second = func(first, element);
+        auto third = func(second, element);
+    }));
+}
 
 
-auto reduce(alias func, Acc, Iter)(in Iter iter, in Acc initial) if(canReduce!Iter){
+
+auto reduce(alias func, Acc, Iter)(in Iter iter, in Acc initial) if(
+    canReduce!(Iter, Acc, func)
+){
     const(Acc)* acc = &initial;
     foreach(element; iter){
         Acc result = func(*acc, element);
@@ -20,10 +34,10 @@ auto reduce(alias func, Acc, Iter)(in Iter iter, in Acc initial) if(canReduce!It
     }
     return *acc;
 }
-auto reduce(alias func, Iter)(in Iter iter) if(canReduce!Iter){
+auto reduce(alias func, Iter)(in Iter iter) if(canReduce!(Iter, func)){
     return reduce!(func, ElementType!Iter, Iter)(iter);
 }
-auto reduce(alias func, Acc, Iter)(in Iter iter) if(canReduce!Iter){
+auto reduce(alias func, Acc, Iter)(in Iter iter) if(canReduce!(Iter, Acc, func)){
     import std.stdio;
     bool first = true;
     const(Acc)* acc;
