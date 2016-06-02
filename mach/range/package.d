4258d78c8584dcc60efc594bb12c96b86e0a7970
Sophie Kirschner <pineapplemachine@gmail.com>
2016-06-02 15:30:02 -0400

    Added new mach.range.logical functions to package

--- mach/range/package.d
+++ mach/range/package.d
@@ -14,7 +14,7 @@ import mach.range.enumerate : enumerate;
 import mach.range.filter : filter;
 import mach.range.indexof : indexof, indexofrange, indexofelement;
 import mach.range.interpolate : interpolate, lerp, coslerp;
-import mach.range.logical : any, all, none, count, exactly, more, less, atleast, atmost;
+import mach.range.logical : any, all, none, first, last, count, exactly, more, less, atleast, atmost;
 import mach.range.map : map;
 import mach.range.mutate : mutate;
 import mach.range.pluck : pluck;
