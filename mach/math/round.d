0455226b0c3f0a71d86e4836fa3f4c0fc9c6146e
Sophie Kirschner <pineapplemachine@gmail.com>
2016-06-03 21:02:48 -0400

    Added a ceil function to mach.math.round
    Intended primarily for getting ceil(x / y) where x and y are both integrals

--- mach/math/round.d
+++ mach/math/round.d
@@ -7,7 +7,9 @@ import std.math : abs;
 
 public:
 
-/// Round to the nearest whole number
+
+
+/// Round to the nearest whole number.
 R round(R = int, N)(in N number) if(isNumeric!N && isNumeric!R){
     static if(isIntegral!N){
         return cast(R) number;
@@ -22,6 +24,25 @@ R round(R = int, N)(in N number) if(isNumeric!N && isNumeric!R){
         }
     }
 }
+
+/// Get the ceiling of one number divided by another.
+R ceil(R = int, N)(in N x, in N y) if(isNumeric!N && isNumeric!R){
+    static if(isIntegral!N){
+        auto floor = x / y;
+        return cast(R) (floor + (floor * y < x));
+    }else{
+        auto result = x / y;
+        if(result % 1 == 0){
+            return cast(R) result;
+        }else if(result >= 0){
+            return cast(R) (result + (1 - (result % 1)));
+        }else{
+            return cast(R) (result - result % 1);
+        }
+    }
+}
+
+
 
 version(unittest) import mach.error.unit;
 unittest{
@@ -47,6 +68,24 @@ unittest{
             testeq(round!real(0.0), 0.0);
             testeq(round!real(0.5), 1.0);
             testeq(round!real(-0.5), -1.0);
+        });
+    });
+    tests("Ceil", {
+        tests("Integers", {
+            testeq(ceil(10, 2), 5);
+            testeq(ceil(10, 3), 4);
+            testeq(ceil(-10, 3), -3);
+            testeq(ceil!real(10, 2), 5.0);
+            testeq(ceil!real(10, 3), 4.0);
+            testeq(ceil!real(-10, 3), -3.0);
+        });
+        tests("Reals", {
+            testeq(ceil(10.0, 2.0), 5);
+            testeq(ceil(10.0, 3.0), 4);
+            testeq(ceil(-10.0, 3.0), -3);
+            testeq(ceil!real(10.0, 2.0), 5.0);
+            testeq(ceil!real(10.0, 3.0), 4.0);
+            testeq(ceil!real(-10.0, 3.0), -3.0);
         });
     });
 }    
