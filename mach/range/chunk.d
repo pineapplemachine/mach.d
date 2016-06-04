b84ba1c5b22e4fd45c1e0bf18127c5c255099663
Sophie Kirschner <pineapplemachine@gmail.com>
2016-06-03 21:17:58 -0400

    Added slicing to mach.range.chunk
    Also saving and more unittests

--- mach/range/chunk.d
+++ mach/range/chunk.d
@@ -4,19 +4,20 @@ private:
 
 import std.traits : isIntegral;
 import mach.math.round : ceil;
-import mach.traits : isSlicingRange;
+import mach.traits : hasNumericLength, isSlicingRange;
 import mach.range.asrange : asrange, validAsSlicingRange;
+import mach.range.meta : MetaRangeMixin;
 
 public:
 
 
 
 enum canChunk(Iter, Count) = (
-    validAsSlicingRange!Iter && validAsChunkCount!Count
+    validAsSlicingRange!Iter && hasNumericLength!Iter && validAsChunkCount!Count
 );
 
 enum canChunkRange(Range, Count) = (
-    isSlicingRange!Range && validAsChunkCount!Count
+    isSlicingRange!Range && hasNumericLength!Range && validAsChunkCount!Count
 );
 
 alias validAsChunkCount = isIntegral;
@@ -30,7 +31,9 @@ alias DefaultChunkCount = size_t;
 /// will be that exact length.
 auto chunk(Iter, Count = DefaultChunkCount)(Iter iter, Count size) if(
     canChunk!(Iter, Count)
-){
+)in{
+    assert(size > 0);
+}body{
     auto range = iter.asrange;
     return ChunkRange!(typeof(range), Count)(range, size);
 }
@@ -48,39 +51,48 @@ auto divide(Iter, Count = DefaultChunkCount)(Iter iter, Count count) if(
 struct ChunkRange(Range, Count = DefaultChunkCount) if(
     canChunkRange!(Range, Count)
 ){
+    mixin MetaRangeMixin!(
+        Range, `source`, `Save`
+    );
+    
     Range source; /// Making chunks from this range
     Count size; /// Maximum size of each chunk
     Count frontindex;
     Count backindex;
     
-    this(Range source, Count size, Count frontindex = Count.init){
+    this(Range source, Count size, Count frontindex = Count.init) in{
+        assert(size > 0);
+    }body{
         this(source, size, frontindex, ceil(source.length, size));
     }
-    this(Range source, Count size, Count frontindex, Count backindex){
+    this(Range source, Count size, Count frontindex, Count backindex) in{
+        assert(size > 0);
+    }body{
         this.source = source;
         this.size = size;
         this.frontindex = frontindex;
         this.backindex = backindex;
     }
     
-    @property auto length(){
-        return ceil(this.source.length, this.size);
-    }
     @property bool empty(){
         return this.frontindex >= this.backindex;
     }
+    @property auto length(){
+        return ceil(this.source.length, this.size);
+    }
+    alias opDollar = length;
     
     @property auto ref front(){
         return this[this.frontindex];
     }
-    void popFront(){
+    void popFront() in{assert(!this.empty);} body{
         this.frontindex++;
     }
     
     @property auto ref back(){
         return this[this.backindex - 1];
     }
-    void popBack(){
+    void popBack() in{assert(!this.empty);} body{
         this.backindex--;
     }
     
@@ -90,6 +102,20 @@ struct ChunkRange(Range, Count = DefaultChunkCount) if(
         if(high > this.source.length) high = this.source.length;
         return this.source[low .. high];
     }
+    
+    typeof(this) opSlice(in Count low, in Count high) in{
+        assert(low >= 0 && high >= low && high <= this.length);
+    }body{
+        if(this.source.length){
+            Count slicelow = low * this.size;
+            Count slicehigh = high * this.size;
+            if(slicelow > this.source.length - 1) slicelow = this.source.length - 1;
+            if(slicehigh > this.source.length) slicehigh = this.source.length;
+            return typeof(this)(this.source[slicelow .. slicehigh], this.size);
+        }else{
+            return typeof(this)(this.source[0 .. $], this.size);
+        }
+    }
 }
 
 
@@ -98,20 +124,50 @@ version(unittest){
     private:
     import mach.error.unit;
     import mach.range.compare : equals;
+    import mach.range.reversed : reversed;
 }
 unittest{
     tests("Chunk", {
         auto input = "abcdefghijklmnop";
         tests("Exact", {
             auto range = input.chunk(4);
-            testeq(range.length, 4);
-            test(range.equals(["abcd", "efgh", "ijkl", "mnop"]));
-            test(range.equals(input.divide(range.length)));
+            testeq("Length", range.length, 4);
+            test("Iteration",
+                range.equals(["abcd", "efgh", "ijkl", "mnop"])
+            );
+            test("Backwards",
+                range.reversed.equals(["mnop", "ijkl", "efgh", "abcd"])
+            );
+            test("Divide",
+                range.equals(input.divide(range.length))
+            );
+            tests("Random access", {
+                test(range[0].equals("abcd"));
+                test(range[1].equals("efgh"));
+                test(range[2].equals("ijkl"));
+                test(range[$-1].equals("mnop"));
+            });
+            tests("Slicing", {
+                test(range[0 .. $].equals(range));
+                test(range[0 .. 1].equals(["abcd"]));
+                test(range[1 .. 3].equals(["efgh", "ijkl"]));
+            });
+            tests("Saving", {
+                auto copy = range.save;
+                copy.popFront();
+                test(range.front.equals("abcd"));
+                test(copy.front.equals("efgh"));
+            });
         });
         tests("Nonexact", {
             auto range = input.chunk(5);
-            testeq(range.length, 4);
-            test(range.equals(["abcde", "fghij", "klmno", "p"]));
+            testeq("Length", range.length, 4);
+            test("Iteration",
+                range.equals(["abcde", "fghij", "klmno", "p"])
+            );
+            test("Backwards",
+                range.reversed.equals(["p", "klmno", "fghij", "abcde"])
+            );
             tests("Divide", {
                 auto input = "abcde";
                 auto range = input.chunk(3);
