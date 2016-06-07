4361ad7e3078a26153ccbcccc246c79c60bb2a93
Sophie Kirschner <pineapplemachine@gmail.com>
2016-06-06 20:07:52 -0400

    Made mach.range.reductions not broken
    Hopefully

--- mach/range/reduction.d
+++ mach/range/reduction.d
@@ -10,10 +10,10 @@ public:
 
 
 template EagerReductionTemplate(alias func, string initial = ``){
-    auto reducefunc(Iter)(in Iter iter) if(canReduceEager!(Iter, func)){
+    auto reducefunc(Iter)(Iter iter) if(canReduceEager!(Iter, func)){
         return reducefunc!(ElementType!Iter, Iter)(iter);
     }
-    auto reducefunc(Acc, Iter)(in Iter iter) if(canReduceEager!(Iter, func)){
+    auto reducefunc(Acc, Iter)(Iter iter) if(canReduceEager!(Iter, func)){
         static if(initial){
             mixin(`return reduceeager!(func, Acc)(iter, ` ~ initial ~ `);`);
         }else{
@@ -26,7 +26,7 @@ template EagerReductionTemplate(alias func, string initial = ``){
 
 
 /// Get the sum of all values in an iterable.
-alias sum = EagerReductionTemplate!((a, b) => (a + b), `Acc.init`);
+alias sum = EagerReductionTemplate!((a, b) => (a + b), `0`);
 /// Get the product of all values in an iterable.
 alias product = EagerReductionTemplate!((a, b) => (a * b));
 
@@ -40,6 +40,7 @@ unittest{
     tests("Summation", {
         testeq([-2, 2].sum, 0);
         testeq([5, 5, 5].sum, 15);
+        testeq([1.0, 2.0].sum, 3.0);
         testeq("Empty series", (new int[0]).sum, 0);
     });
     tests("Product", {
