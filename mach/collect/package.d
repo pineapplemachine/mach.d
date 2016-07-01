0f16dd48e73bb59d9588f946a6346a0d59966ab5
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-01 15:41:57 -0400

    Added aslist import to mach.collect package.d

--- mach/collect/package.d
+++ mach/collect/package.d
@@ -3,5 +3,5 @@ module mach.collect;
 public:
 
 import mach.collect.heap : Heap, heapify;
-import mach.collect.linkedlist : LinkedList;
+import mach.collect.linkedlist : LinkedList, aslist;
 import mach.collect.sortedlist : SortedList;
