410be80026a2fa41730f797de30ec5fe3b315473
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-21 21:31:21 -0400

    Added mach.meta.repeat

--- mach/meta/package.d
+++ mach/meta/package.d
@@ -6,3 +6,4 @@ import mach.meta.aliases : Alias, Aliases;
 import mach.meta.filter : Filter;
 import mach.meta.logical : Any, All, None, Count, First, Last;
 import mach.meta.map : Map;
+import mach.meta.repeat : Repeat;
---
+++ mach/meta/repeat.d
@@ -0,0 +1,28 @@
+module mach.meta.repeat;
+
+private:
+
+import mach.meta.aliases : Aliases;
+
+public:
+
+
+
+template Repeat(size_t count, T...){
+    static if(count == 0){
+        alias Repeat = Aliases!();
+    }else static if(count == 1){
+        alias Repeat = T;
+    }else{
+        alias Repeat = Aliases!(T, Repeat!(count - 1, T));
+    }
+}
+
+
+
+unittest{
+    static assert(is(Repeat!(0, int, long) == Aliases!()));
+    static assert(is(Repeat!(1, int, long) == Aliases!(int, long)));
+    static assert(is(Repeat!(2, int, long) == Aliases!(int, long, int, long)));
+    static assert(is(Repeat!(3, int, long) == Aliases!(int, long, int, long, int, long)));
+}
