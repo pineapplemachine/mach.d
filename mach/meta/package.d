f12cc90fb8a2b8b5dabf4390cb330796b977e97d
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-28 17:57:37 -0400

    Added mach.meta.partial

--- mach/meta/package.d
+++ mach/meta/package.d
@@ -6,4 +6,5 @@ import mach.meta.aliases : Alias, Aliases;
 import mach.meta.filter : Filter;
 import mach.meta.logical : Any, All, None, Count, First, Last;
 import mach.meta.map : Map;
+import mach.meta.partial : Partial, PartialFront, PartialBack;
 import mach.meta.repeat : Repeat;
