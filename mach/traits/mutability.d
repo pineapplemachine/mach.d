a7a0114cc78c1106b95ef31262ea9c715e7199b2
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-29 16:22:42 -0400

    More unittests and general improvements to mach.traits.mutability

--- mach/traits/mutability.d
+++ mach/traits/mutability.d
@@ -8,8 +8,9 @@ public:
 
 
 
+enum isMutable(alias T) = isMutable!(typeof(T));
 template isMutable(T){
-    enum bool isMutable = is(typeof((inout int = 0){
+    enum bool isMutable = is(typeof((){
         T value = T.init;
         value = T.init;
     }));
@@ -18,7 +19,17 @@ template isMutable(T){
 
 
 unittest{
-    // TODO: More tests
+    struct MutMember{int x;}
+    struct ConstMember{const int x;}
+    int x; const int cx;
+    static assert(isMutable!x);
     static assert(isMutable!int);
+    static assert(isMutable!string);
+    static assert(isMutable!MutMember);
+    static assert(!isMutable!cx);
     static assert(!isMutable!(const int));
+    static assert(!isMutable!(immutable char));
+    static assert(!isMutable!(const MutMember));
+    static assert(!isMutable!ConstMember);
+    static assert(!isMutable!(typeof(ConstMember[].init[0])));
 }
