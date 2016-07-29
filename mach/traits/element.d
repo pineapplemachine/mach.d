6a48f157f0535e5617d3004cb01340718de58ef6
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-29 16:31:57 -0400

    Cleaning up mach.traits.element
    Still kind of a WIP. I think I'm going to just make it its own package with a handful of separate modules. Also needs better unittest coverage.

--- mach/traits/element.d
+++ mach/traits/element.d
@@ -3,8 +3,9 @@ module mach.traits.element;
 private:
 
 import std.meta : staticMap;
-import std.traits : isArray, isDelegate, isSomeFunction, fullyQualifiedName;
-import std.traits : Parameters, ReturnType;
+import std.traits : isArray, fullyQualifiedName;
+import std.traits : Parameters;
+import mach.traits.call : isDelegate, isCallableWith;
 import mach.traits.common : hasCommonType, CommonType;
 import mach.traits.hash : canHash;
 import mach.traits.iter : isRange, isIterable;
@@ -16,18 +17,34 @@ public:
 
 
 
+/// Get the element type of an array.
+//template ArrayElementType(alias Array) if(isArray!(typeof(Array))){
+//    alias ArrayElementType = ArrayElementType!(typeof(Array));
+//}
+/// ditto
 template ArrayElementType(Array) if(isArray!Array){
     alias ArrayElementType = typeof(Array.init[0]);
 }
 
+
+
+/// Get the element type of a range.
+//template RangeElementType(alias Range) if(isRange!(typeof(Range))){
+//    alias RangeElementType = RangeElementType!(typeof(Range));
+//}
+/// ditto
 template RangeElementType(Range) if(isRange!Range){
-    static if(isSomeFunction!(Range.init.front)){
-        alias RangeElementType = ReturnType!(Range.init.front);
+    static if(isCallableWith!(Range.init.front)){
+        alias RangeElementType = typeof(Range.init.front());
     }else{
         alias RangeElementType = typeof(Range.init.front);
     }
 }
 
+
+
+/// Base template for getting the element type of some type implementing opApply
+/// or opApplyReverse.
 private template OpApplyGenericElementType(Iter, alias op){
     alias ApplyParams = Parameters!op;
     static assert(
@@ -35,26 +52,47 @@ private template OpApplyGenericElementType(Iter, alias op){
         fullyQualifiedName!Iter ~ " has invalid arguments for opApply."
     );
     alias Delegate = ApplyParams[0];
+    // TODO: What if there's more than one opApply?
+    // Probably going to need a more robust Parameters template for that.
     alias DelegateParams = Parameters!Delegate;
     static assert(DelegateParams.length == 1);
     alias OpApplyGenericElementType = DelegateParams[0];
 }
+
+/// Get the element type of a type implementing opApply.
+//template OpApplyElementType(alias Iter) if(hasOpApply!(typeof(Iter))){
+//    alias OpApplyElementType = OpApplyElementType!(typeof(Iter));
+//}
+/// ditto
 template OpApplyElementType(Iter) if(hasOpApply!Iter){
     alias OpApplyElementType = OpApplyGenericElementType!(Iter, Iter.opApply);
 }
+
+/// Get the element type of a type implementing opApplyReverse.
+//template OpApplyReverseElementType(alias Iter) if(hasOpApplyReverse!(typeof(Iter))){
+//    alias OpApplyReverseElementType = OpApplyReverseElementType!(typeof(Iter));
+//}
+/// ditto
 template OpApplyReverseElementType(Iter) if(hasOpApplyReverse!Iter){
     alias OpApplyReverseElementType = OpApplyGenericElementType!(Iter, Iter.opApplyReverse);
 }
 
 
 
-/// Get the element type of anything that can be iterated.
+/// Determine if a type has an Element alias member.
+template hasElementAlias(Tx...) if(Tx.length == 1){
+    enum bool hasElementAlias = __traits(compiles, {alias E = Iter.Element;});
+}
+
+
+
+/// Get the element type of some iterable type.
 template ElementType(Iter){
     static if(isArray!Iter){
         alias ElementType = ArrayElementType!Iter;
     }else static if(isRange!Iter){
         alias ElementType = RangeElementType!Iter;
-    }else static if(__traits(compiles, {alias E = Iter.Element;})){
+    }else static if(hasElementAlias!Iter){
         alias ElementType = Iter.Element;
     }else static if(hasOpApply!Iter){
         alias ElementType = OpApplyElementType!Iter;
@@ -115,58 +153,54 @@ version(unittest){
     private:
     struct RangeElementTest{
         int value;
-        @property auto front() const{
-            return this.value;
-        }
-        void popFront(){
-            this.value++;
-        }
+        @property auto front() const{return this.value;}
+        void popFront(){this.value++;}
         enum bool empty = false;
     }
     struct ApplyElementTest{
-        int opApply(in int delegate(ref string item) each){
-            return 0;
-        }
+        int opApply(in int delegate(ref string) each){return 0;}
     }
     struct ApplyReverseElementTest{
-        int opApplyReverse(in int delegate(ref string item) each){
-            return 0;
-        }
+        int opApplyReverse(in int delegate(ref string) each){return 0;}
     }
 }
+
 unittest{
-    // Arrays
+    static assert(is(ArrayElementType!(int[]) == int));
+    static assert(is(ArrayElementType!(real[][]) == real[]));
+    static assert(is(ArrayElementType!(const int[]) == const int));
+    static assert(is(ArrayElementType!(immutable int[]) == immutable int));
+}
+
+unittest{
     static assert(is(ElementType!(int[]) == int));
     static assert(is(ElementType!(real[][]) == real[]));
-    // Ranges
     static assert(is(ElementType!RangeElementTest == const(int)));
-    // OpApply
     static assert(is(ElementType!ApplyElementTest == string));
-    // OpApplyReverse
     static assert(is(ElementType!ApplyReverseElementTest == string));
 }
+
 unittest{
-    // isIterableOf type
+    // Type
     static assert(isIterableOf!(int[], int));
     static assert(isIterableOf!(string, immutable char));
     static assert(!isIterableOf!(int, int));
-    // isIterableOf predicate
+    // Predicate
     static assert(isIterableOf!(int[][], isArray));
     static assert(!isIterableOf!(int[], isArray));
     static assert(!isIterableOf!(int, isArray));
 }
+
 unittest{
-    // hasCommonElementType
     static assert(hasCommonElementType!(int[], real[]));
     static assert(hasCommonElementType!(string[], ApplyElementTest));
     static assert(!hasCommonElementType!(int[], string[]));
     static assert(!hasCommonElementType!(int[], ApplyElementTest));
-    // CommonElementType
     static assert(is(CommonElementType!(int[], real[]) == real));
     static assert(is(CommonElementType!(string[], ApplyElementTest) == string));
 }
+
 unittest{
-    // isElementPredicate
     alias even = (n) => (n % 2 == 0);
     alias index = (str) => (str[0] == '?');
     static assert(isElementPredicate!(even, int[]));
@@ -174,7 +208,8 @@ unittest{
     static assert(isElementPredicate!(index, string[]));
     static assert(!isElementPredicate!(even, string[]));
     static assert(!isElementPredicate!(index, int[]));
-    // isElementTransformation
+}
+unittest{
     alias twice = (n) => (n + n);
     alias sum = (a, b) => (a + b);
     static assert(isElementTransformation!(twice, int[]));
