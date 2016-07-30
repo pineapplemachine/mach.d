3cc697419c7181da8635658f9dc98728bd012d8a
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-30 09:24:36 -0400

    More docs and unittests in mach.traits.call.callable

--- mach/traits/call/callable.d
+++ mach/traits/call/callable.d
@@ -9,6 +9,8 @@ public:
 
 
 
+/// Determine whether a type is callable. This includes functions, delegates,
+/// and classes and structs implementing opCall.
 template isCallable(Tx...) if(Tx.length == 1){
     enum bool isCallable = !is(CallableType!Tx == void);
 }
@@ -25,4 +27,29 @@ template CallableType(Tx...) if(Tx.length == 1){
     }else{
         alias CallableType = void;
     }
+}
+
+
+
+unittest{
+    int x;
+    int call1(){return 0;}
+    void call2(){}
+    static void call3(){}
+    struct call4{static void opCall(int){}}
+    void function(int) call5;
+    void delegate(int) call6;
+    static assert(isCallable!(typeof(call1)));
+    static assert(isCallable!call1);
+    static assert(isCallable!call2);
+    static assert(isCallable!call3);
+    static assert(isCallable!call4);
+    static assert(isCallable!call5);
+    static assert(isCallable!call6);
+    static assert(is(CallableType!call1 == typeof(call1)));
+    static assert(is(CallableType!call2 == typeof(call2)));
+    static assert(is(CallableType!call3 == typeof(call3)));
+    static assert(is(CallableType!call4 == typeof(call4.opCall)));
+    //static assert(is(CallableType!call5 == typeof(call5))); // TODO: Doesn't work
+    static assert(is(CallableType!call6 == typeof(call6)));
 }
