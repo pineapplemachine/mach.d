92251875f15363ec51739dc43bafbfd172cdfbf4
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-30 09:24:22 -0400

    Improved mach.traits.array
    At some point I'd like to make this module not rely on phobos' isArray but when I checked out the source it looked like a monster of a thing. Will probably take some time to comprehend.

--- mach/traits/array.d
+++ mach/traits/array.d
@@ -2,16 +2,42 @@ module mach.traits.array;
 
 private:
 
-import std.traits : isArray;
-import mach.traits.element : ArrayElementType;
+//
 
 public:
 
-enum isArrayOf(Array, Element) = (
-    isArray!Array && is(ArrayElementType!Array == Element)
-);
+
+
+/// Determine whether some type is an array.
+enum bool isArray(alias T) = isArray!(typeof(T));
+/// ditto
+template isArray(T){
+    static import std.traits;
+    enum bool isArray = std.traits.isArray!T;
+}
+
+/// Determine whether some type is an array of a specific element type.
+enum isArrayOf(alias Array, Element) = isArrayOf!(typeof(Array), Element);
+/// ditto
+template isArrayOf(Array, Element){
+    import mach.traits.element.array : ArrayElementType;
+    enum bool isArrayOf = (
+        isArray!Array && is(ArrayElementType!Array == Element)
+    );
+}
+
+
 
 unittest{
+    int[] ints;
+    static assert(isArray!ints);
+    static assert(isArray!(int[]));
+    static assert(isArray!string);
+    static assert(!isArray!int);
+}
+unittest{
+    int[] ints;
+    static assert(isArrayOf!(ints, int));
     static assert(isArrayOf!(int[], int));
     static assert(isArrayOf!(int[][], int[]));
     static assert(!isArrayOf!(int[], real));
