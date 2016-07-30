6d493935724a4f453a6b41872a13e51536dc4b57
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-30 09:25:02 -0400

    Added a nice sheen of polish to mach.traits.hash

--- mach/traits/hash.d
+++ mach/traits/hash.d
@@ -6,14 +6,41 @@ private:
 
 public:
 
+
+
+/// Determine whether it's possible to get the hash of some type.
+enum canHash(alias T) = canHash!(typeof(T));
+/// ditto
 template canHash(T){
-    enum bool canHash = is(typeof((inout int = 0){
-        T thing = T.init;
-        typeid(thing).getHash(&thing);
+    enum bool canHash = is(typeof({
+        T value = T.init;
+        typeid(value).getHash(&value);
     }));
 }
 
+
+
+/// Get the hash of some value.
+auto hash(T)(auto ref T value) if(canHash!T){
+    return typeid(value).getHash(&value);
+}
+
+
+
 unittest{
-    // TODO: more tests
+    string str;
+    static assert(canHash!str);
     static assert(canHash!string);
+    static assert(canHash!int);
+    static assert(!canHash!void);
+}
+
+unittest{
+    string str0 = "hello world";
+    string str1 = "hello world";
+    string str2 = "hello worlds";
+    assert(str0.hash == str1.hash);
+    assert(str0.hash != str2.hash);
+    assert(0.hash == 0.hash);
+    assert(0.hash != 1.hash);
 }
