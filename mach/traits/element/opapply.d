4a3f349517d030a66c582f17b6b88e2fa4259559
Sophie Kirschner <pineapplemachine@gmail.com>
2016-07-29 20:43:12 -0400

    Made mach.traits.element.opapply even better
    Can now work with opApply implementations that take more than one argument (the element type will be represented as an arguments tuple)

--- mach/traits/element/opapply.d
+++ mach/traits/element/opapply.d
@@ -19,11 +19,14 @@ private template OpApplyGenericElementType(alias op){
         "Invalid arguments for opApply."
     );
     alias Delegate = ApplyParams[0];
-    // TODO: What if there's more than one opApply?
+    // TODO: What if there's more than one opApply overload?
     // Probably going to need a more robust Parameters template for that.
     alias DelegateParams = Parameters!Delegate;
-    static assert(DelegateParams.length == 1);
-    alias OpApplyGenericElementType = DelegateParams[0];
+    static if(DelegateParams.length == 1){
+        alias OpApplyGenericElementType = DelegateParams[0];
+    }else{
+        alias OpApplyGenericElementType = DelegateParams;
+    }
 }
 
 
@@ -40,11 +43,17 @@ template OpApplyReverseElementType(Tx...) if(Tx.length == 1 && hasOpApplyReverse
 
 
 
+version(unittest){
+    private:
+    import mach.meta : Aliases;
+}
 unittest{
     struct ApplyInt{int opApply(int delegate(ref int)){return 0;}}
     struct ApplyString{int opApply(int delegate(ref string)){return 0;}}
+    struct ApplyTuple{int opApply(int delegate(ref int, ref int)){return 0;}}
     struct ApplyReverseInt{int opApplyReverse(int delegate(ref int)){return 0;}}
     struct ApplyReverseString{int opApplyReverse(int delegate(ref string)){return 0;}}
+    struct ApplyReverseTuple{int opApplyReverse(int delegate(ref int, ref int)){return 0;}}
     struct ApplyBidirectional{
         int opApply(int delegate(ref int)){return 0;}
         int opApplyReverse(int delegate(ref int)){return 0;}
@@ -54,9 +63,11 @@ unittest{
     static assert(is(OpApplyElementType!apply == int));
     static assert(is(OpApplyElementType!ApplyInt == int));
     static assert(is(OpApplyElementType!ApplyString == string));
+    static assert(is(OpApplyElementType!ApplyTuple == Aliases!(int, int)));
     static assert(is(OpApplyElementType!ApplyBidirectional == int));
     static assert(is(OpApplyReverseElementType!applyrev == int));
     static assert(is(OpApplyReverseElementType!ApplyReverseInt == int));
     static assert(is(OpApplyReverseElementType!ApplyReverseString == string));
+    static assert(is(OpApplyReverseElementType!ApplyReverseTuple == Aliases!(int, int)));
     static assert(is(OpApplyReverseElementType!ApplyBidirectional == int));
 }
