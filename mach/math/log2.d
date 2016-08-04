7052fe3e6151a173458af518c1afd14cf00dbada
Sophie Kirschner <pineapplemachine@gmail.com>
2016-08-04 15:42:34 -0400

    Added mach.math.log2

---
+++ mach/math/log2.d
@@ -0,0 +1,60 @@
+module mach.math.log2;
+
+private:
+
+import std.traits : Unqual, isIntegral;
+
+public:
+
+
+
+// Credit http://stackoverflow.com/a/22701843/4099022
+
+
+
+/// Get ceil(log2(n)) of some integer.
+auto clog2(N)(N number) if(isIntegral!N) in{
+    assert(number > 0);
+}body{
+    Unqual!N log, n = number - 1;
+    while(n > 0){
+        log++; n >>= 1;
+    }
+    return log;
+}
+
+/// Get floor(log2(n)) of some integer.
+auto flog2(N)(N number) if(isIntegral!N) in{
+    assert(number > 0);
+}body{
+    Unqual!N log, n = number;
+    while(n > 1){
+        log++; n >>= 1;
+    }
+    return log;
+}
+
+
+
+version(unittest){
+    private:
+    import mach.error.unit;
+}
+unittest{
+    tests("Log2", {
+        tests("Floor", {
+            testeq(flog2(1), 0);
+            testeq(flog2(2), 1);
+            testeq(flog2(4), 2);
+            testeq(flog2(8), 3);
+            testeq(flog2(9), 3);
+        });
+        tests("Ceil", {
+            testeq(clog2(1), 0);
+            testeq(clog2(2), 1);
+            testeq(clog2(4), 2);
+            testeq(clog2(8), 3);
+            testeq(clog2(9), 4);
+        });
+    });
+}
