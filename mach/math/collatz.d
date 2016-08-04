3fea3d315c49c7faafe1b649b1785bc8a91d3bb9
Sophie Kirschner <pineapplemachine@gmail.com>
2016-08-04 16:32:47 -0400

    Restated collatz and juggler sequence ranges using mach.range.recur

--- mach/math/collatz.d
+++ mach/math/collatz.d
@@ -3,6 +3,7 @@ module mach.math.collatz;
 private:
 
 import std.traits : isIntegral;
+import mach.range : recur;
 
 public:
 
@@ -14,38 +15,13 @@ alias canCollatz = isIntegral;
 
 
 /// Get a range which enumerates the Collatz sequence of a given number.
-auto collatzseq(N)(N value) if(canCollatz!N){
-    return CollatzRange!N(value);
-}
-
-
-
-/// Enumerates the Collatz sequence of an input number.
-struct CollatzRange(N) if(canCollatz!N){
-    N front;
-    bool empty;
-    
-    this(N front, bool empty = false) in{
-        assert(front >= 1, "Operation is only meaningful for positive integers.");
-    }body{
-        this.front = front;
-        this.empty = empty;
-    }
-    
-    void popFront(){
-        if(this.front == 1){
-            this.empty = true;
-        }else if(this.front % 2 == 0){
-            this.front /= 2;
-        }else{
-            this.front *= 3;
-            this.front++;
-        }
-    }
-    
-    @property typeof(this) save(){
-        return typeof(this)(this.front, this.empty);
-    }
+auto collatzseq(N)(N value) if(canCollatz!N) in{
+    assert(value >= 1, "Operation is only meaningful for positive integers.");
+}body{
+    return value.recur!(
+        (in N n) => (n % 2 == 0 ? n / 2 : n * 3 + 1),
+        (in N n) => (n <= 1), true
+    );
 }
 
 
--- mach/math/juggler.d
+++ mach/math/juggler.d
@@ -4,12 +4,9 @@ private:
 
 import std.traits : isIntegral, isFloatingPoint;
 import std.math : pow;
+import mach.range : recur;
 
 public:
-
-
-
-// Reference: https://en.wikipedia.org/wiki/Juggler_sequence
 
 
 
@@ -19,36 +16,14 @@ enum canJuggler(N, F = real) = isIntegral!N && isFloatingPoint!F;
 
 
 /// Get a range which enumerates the juggler sequence of a given number.
-auto jugglerseq(N, F = real)(N value) if(canJuggler!(N, F)){
-    return JugglerRange!(N, F)(value);
-}
-
-
-
-/// Enumerates the juggler sequence of an input number. N will be the type of
-/// each element and F an interim floating point type used to calculate each step.
-struct JugglerRange(N, F) if(canJuggler!(N, F)){
-    N front;
-    bool empty;
-    
-    this(N front, bool empty = false) in{
-        assert(front >= 1, "Operation is only meaningful for positive integers.");
-    }body{
-        this.front = front;
-        this.empty = empty;
-    }
-    
-    void popFront(){
-        if(this.front == 1){
-            this.empty = true;
-        }else{
-            this.front = cast(N)(cast(F) this.front.pow(this.front % 2 == 0 ? 0.5 : 1.5));
-        }
-    }
-    
-    @property typeof(this) save(){
-        return typeof(this)(this.front, this.empty);
-    }
+/// Reference: https://en.wikipedia.org/wiki/Juggler_sequence
+auto jugglerseq(N, F = real)(N value) if(canJuggler!(N, F)) in{
+    assert(value >= 1, "Operation is only meaningful for positive integers.");
+}body{
+    return value.recur!(
+        (in N n) => (cast(N)(cast(F) n.pow(n % 2 == 0 ? 0.5 : 1.5))),
+        (in N n) => (n <= 1), true
+    );
 }
 
 
