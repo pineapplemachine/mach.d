5a58bbf56621d287f9672a09fe87b74a25897852
Sophie Kirschner <pineapplemachine@gmail.com>
2016-08-04 20:06:31 -0400

    Added package.d in mach.math

---
+++ mach/math/package.d
@@ -0,0 +1,9 @@
+module mach.math;
+
+import mach.math.box;
+import mach.math.cube;
+import mach.math.log2;
+import mach.math.matrix4;
+import mach.math.round;
+import mach.math.vector2;
+import mach.math.vector3;
