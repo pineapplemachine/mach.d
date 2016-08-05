18946b9a31c6ec5af4c128ac659b6082a4764ba4
Sophie Kirschner <pineapplemachine@gmail.com>
2016-08-04 21:15:07 -0400

    Added root package.d for the library

---
+++ mach/package.d
@@ -0,0 +1,14 @@
+module mach;
+
+public:
+
+import mach.collect;
+import mach.error;
+import mach.io;
+import mach.math;
+import mach.meta;
+//import mach.misc; // No package.d as of yet
+import mach.range;
+//import mach.sdl; // Unstable!
+import mach.text;
+import mach.traits;
