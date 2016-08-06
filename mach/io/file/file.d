46e6feed740aa7b7d80d7e405187727b8dd3e818
Sophie Kirschner <pineapplemachine@gmail.com>
2016-08-06 09:42:18 -0400

    Added docs to mach.io.file.file
    They're not GREAT docs but hey at least it's something

--- mach/io/file/file.d
+++ mach/io/file/file.d
@@ -8,19 +8,25 @@ public:
 
 
 
-class File{
+/// Provides clean syntax for initializing file streams.
+abstract class File{
+    /// Open the file at a path.
     static auto open(string path, in char[] mode = "rb"){
         return new FileStream(path, mode);
     }
+    /// Open the file at a path in read mode.
     static auto read(string path){
         return File.open(path, "rb");
     }
+    /// Open the file at a path in write mode.
     static auto write(string path){
         return File.open(path, "w+b");
     }
+    /// Open the file at a path in append mode.
     static auto append(string path){
         return File.open(path, "ab");
     }
+    /// Create and open a new temporary file.
     static auto temp(){
         return FileStream.temp();
     }
