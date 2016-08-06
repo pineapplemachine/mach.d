313779e453310121be72cf1155341f5e75232812
Sophie Kirschner <pineapplemachine@gmail.com>
2016-08-06 07:45:20 -0400

    Added module mach.sys.endian

---
+++ mach/sys/endian.d
@@ -0,0 +1,39 @@
+module mach.sys.endian;
+
+private:
+
+//
+
+public:
+
+
+
+enum Endian{
+    BigEndian, LittleEndian
+}
+
+
+
+T endianswap(T)(in T value) pure nothrow @nogc @trusted{
+    T swapped = void;
+    ubyte* src = cast(ubyte*) &value;
+    ubyte* dst = (cast(ubyte*) &swapped) + T.sizeof - 1;
+    foreach(_; 0 .. T.sizeof){
+        *dst = *src;
+        src++;
+        dst--;
+    }
+    return swapped;
+}
+
+
+
+version(unittest) private import mach.error.unit;
+unittest{
+    tests("Endian swap", {
+        testeq(endianswap(ubyte(0xff)), 0xff);
+        testeq(endianswap(ushort(0x00ff)), 0xff00);
+        testeq(endianswap(uint(0x01020304)), 0x04030201);
+        testeq(endianswap(ulong(0x0102030405060708)), 0x0807060504030201);
+    });
+}
---
+++ mach/sys/package.d
@@ -0,0 +1,5 @@
+module mach.sys;
+
+public:
+
+import mach.sys.endian;
