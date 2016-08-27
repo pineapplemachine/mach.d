e23425fa41719077802000a41d37c58c9ec45784
Sophie Kirschner <pineapplemachine@gmail.com>
2016-08-27 09:29:30 -0400

    Additional functionality in mach.sdl.primitives

--- mach/sdl/primitives.d
+++ mach/sdl/primitives.d
@@ -5,6 +5,8 @@ private:
 import derelict.sdl2.sdl;
 import derelict.opengl3.gl;
 
+import mach.meta : All;
+import mach.traits : isTemplateOf;
 import mach.math.vector2 : Vector2;
 import mach.math.vector3 : Vector3;
 
@@ -19,36 +21,87 @@ public:
 
 
 
+void glVertex(T)(T x, T y){
+    static if(is(T == short)) alias impl = glVertex2s;
+    else static if(is(T == int)) alias impl = glVertex2i;
+    else static if(is(T == float)) alias impl = glVertex2f;
+    else static if(is(T == double)) alias impl = glVertex2d;
+    else alias impl = void;
+    static if(!is(impl == void)){
+        impl(x, y);
+    }else static if(isIntegral!T){
+        glVertex3i(cast(int) x, cast(int) y);
+    }else{
+        glVertex3d(cast(double) x, cast(double) y);
+    }
+}
+void glVertex(T)(T x, T y, T z){
+    static if(is(T == short)) alias impl = glVertex3s;
+    else static if(is(T == int)) alias impl = glVertex3i;
+    else static if(is(T == float)) alias impl = glVertex3f;
+    else static if(is(T == double)) alias impl = glVertex3d;
+    else alias impl = void;
+    static if(!is(impl == void)){
+        impl(x, y, z);
+    }else static if(isIntegral!T){
+        glVertex3i(cast(int) x, cast(int) y, cast(int) z);
+    }else{
+        glVertex3d(cast(double) x, cast(double) y, cast(double) z);
+    }
+}
+
+
 void glset(T)(Vector2!T vector){
-    static if(is(T == short)) alias glVertex = glVertex2s;
-    else static if(is(T == int)) alias glVertex = glVertex2i;
-    else static if(is(T == float)) alias glVertex = glVertex2f;
-    else static if(is(T == double)) alias glVertex = glVertex2d;
-    else static assert(false);
-    glVertex(vector.x, vector.y);
+    glVertex!T(vector.x, vector.y);
 }
 
 void glset(T)(Vector3!T vector){
-    static if(is(T == short)) alias glVertex = glVertex3s;
-    else static if(is(T == int)) alias glVertex = glVertex3i;
-    else static if(is(T == float)) alias glVertex = glVertex3f;
-    else static if(is(T == double)) alias glVertex = glVertex3d;
-    else static assert(false);
-    glVertex(vector.x, vector.y, vector.z);
+    glVertex!T(vector.x, vector.y, vector.z);
 }
 
+enum isVector2(T) = isTemplateOf!(T, Vector2);
 
 
-auto line(A, B, C)(Window window, Vector2!A v0, Vector2!B v1, Color!C color, float width = 1){
-    scope(exit) GLError.enforce();
-    window.use();
-    color.glset();
+
+auto primitives(alias mode, C, Vectors...)(Color!C color, Vectors vectors) if(
+    All!(isVector2, Vectors)
+){
+    static if(vectors.length){
+        scope(exit) GLError.enforce();
+        color.glset();
+        glBegin(mode);
+        foreach(vector; vectors) vector.glset();
+        glEnd();
+    }
+}
+//auto primitives(alias mode, C, T)(Color!C color, Vector2!T[] vectors){
+//    if(vectors && vectors.length){
+//        scope(exit) GLError.enforce();
+//        color.glset();
+//        glBegin(mode);
+//        foreach(vector; vectors) vector.glset();
+//        glEnd();
+//    }
+//}
+
+
+
+auto points(C, Vectors...)(Color!C color, Vectors vectors) if(
+    All!(isVector2, Vectors)
+){
+    primitives!(GL_POINTS, C, Vectors)(color, vectors);
+}
+
+auto lines(C, Vectors...)(Color!C color, float width, Vectors vectors) if(
+    All!(isVector2, Vectors)
+)in{
+    assert(vectors.length % 2 == 0);
+}body{
     glLineWidth(width);
-    glBegin(GL_LINES);
-    v0.glset();
-    v1.glset();
-    glEnd();
+    primitives!(GL_LINES, C, Vectors)(color, vectors);
 }
+
+
 
 
 
