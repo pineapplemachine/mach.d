413e2b340ac0188212f9022033eb461a6ed7e9a5
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-01 15:46:15 -0400

    mach.range.unique now uses DenseHashSet instead of an AA
    I should probably do some performance comparisons just to make sure it's actually faster

--- mach/range/unique.d
+++ mach/range/unique.d
@@ -2,37 +2,59 @@ module mach.range.unique;
 
 private:
 
-import mach.traits : canHash, ElementType, isIterable;
+import mach.traits : canHash, ElementType, isIterable, hasNumericLength;
+import mach.collect : DenseHashSet;
 
 public:
 
 
 
+/// Determine whether a `by` function is valid for some iterable.
 enum validUniqueBy(Iter, alias by) = (
     canHash!(typeof(by(ElementType!Iter.init)))
 );
 
+/// Determine whether it's possible to call unique given an iterable type and a
+/// `by` function.
 enum canUnique(Iter, alias by = DefaultUniqueBy) = (
     isIterable!Iter && validUniqueBy!(Iter, by)
 );
 
+/// The default `by` function.
 alias DefaultUniqueBy = (element) => (element);
+
+
+
+/// Default for makehistory argument of unique function. Given an iterable and
+/// a `by` function, any function passed for that argument should return an
+/// object supporting both `history.add(element)` and `element in history`
+/// syntax, such as a set.
+auto DefaultUniqueMakeHistory(alias by, Iter)(auto ref Iter iter){
+    alias ByType = typeof(by(ElementType!Iter.init));
+    enum hasLength = hasNumericLength!Iter;
+    DenseHashSet!(ByType, !hasLength) history;
+    static if(hasLength) history.reserve(iter.length * 4);
+    return history;
+}
 
 
 
 /// Return true when all elements of the iterable are unique, as determined by
 /// hashing and equality functions. Return true when the iterable is empty.
 /// Evaluates eagerly.
-auto unique(alias by = DefaultUniqueBy, Iter)(auto ref Iter iter) if(canUnique!(Iter, by)){
-    alias ByType = typeof(by(ElementType!Iter.init));
-    alias History = bool[ByType]; // TODO: Proper set?
-    History history;
+/// When a `by` function is specified, uniqueness of elements is determined by
+/// the uniqueness of the result of calling that function for each element.
+auto unique(alias by = DefaultUniqueBy, alias makehistory = DefaultUniqueMakeHistory, Iter)(
+    auto ref Iter iter
+) if(canUnique!(Iter, by)){
+    
+    auto history = makehistory!by(iter);
     foreach(element; iter){
         auto byelement = by(element);
         if(byelement in history){
             return false;
         }else{
-            history[byelement] = true;
+            history.add(byelement);
         }
     }
     return true;
