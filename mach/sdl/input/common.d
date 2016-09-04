1074d37c9c20df6bc98d294464a2f8a848723b2b
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-04 17:41:56 -0400

    Moved definition of SDL Timestamp type into mach.sdl.input.common

--- mach/sdl/input/common.d
+++ mach/sdl/input/common.d
@@ -19,3 +19,13 @@ enum ButtonState{
     Pressed = SDL_PRESSED, // 1
 }
 
+/// Timestamps measure the number of milliseconds since the SDL library
+/// was initialized.
+/// Timestamps of events represent when SDL_PumpEvents was last called and
+/// not necessarily exactly when those events actually occurred.
+/// References:
+/// https://forums.libsdl.org/viewtopic.php?p=38148&sid=316e65d429d7668b40c879b105fa0b93
+/// https://github.com/ioquake/ioq3/issues/215
+alias Timestamp = uint;
+
+
--- mach/sdl/input/event/event.d
+++ mach/sdl/input/event/event.d
@@ -31,14 +31,7 @@ public:
 
 
 struct Event{
-    /// Timestamps measure the number of milliseconds since the SDL library
-    /// was initialized.
-    /// Timestamps of events represent when SDL_PumpEvents was last called and
-    /// not necessarily exactly when those events actually occurred.
-    /// References:
-    /// https://forums.libsdl.org/viewtopic.php?p=38148&sid=316e65d429d7668b40c879b105fa0b93
-    /// https://github.com/ioquake/ioq3/issues/215
-    alias Timestamp = uint;
+    alias Timestamp = .Timestamp;
     /// https://wiki.libsdl.org/SDL_Event
     alias Type = EventType;
     
