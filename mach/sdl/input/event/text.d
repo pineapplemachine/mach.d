6603a7c52d849eac5243ed073f2daedc6df4751d
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-03 20:37:54 -0400

    Added methods to TextInputEvent
    Not sure if this is a good place for them but they've got to go somewhere I suppose

--- mach/sdl/input/event/text.d
+++ mach/sdl/input/event/text.d
@@ -2,7 +2,7 @@ module mach.sdl.input.event.text;
 
 private:
 
-import derelict.sdl2.types;
+import derelict.sdl2.sdl;
 import mach.sdl.input.event.mixins;
 
 public:
@@ -14,6 +14,31 @@ public:
 struct TextInputEvent{
     mixin EventMixin!SDL_TextInputEvent;
     mixin TextEventMixin;
+    
+    // TODO: Is this really the right place to put these methods?
+    // Seems a little out of the way
+    
+    /// Get whether text input events are currently enabled.
+    /// https://wiki.libsdl.org/SDL_IsTextInputActive
+    static bool enabled(){
+        return cast(bool) SDL_IsTextInputActive();
+    }
+    /// Start receiving text input events.
+    /// https://wiki.libsdl.org/SDL_StartTextInput
+    static void start(){
+        SDL_StartTextInput();
+    }
+    /// Stop receiving text input events.
+    /// https://wiki.libsdl.org/SDL_StopTextInput
+    static void stop(){
+        SDL_StopTextInput();
+    }
+    /// Controls where the candidate list will open, if supported.
+    /// https://wiki.libsdl.org/SDL_SetTextInputRect
+    static @property void inputrect(int x, int y, int w, int h){
+        auto rect = SDL_Rect(x, y, w, h);
+        SDL_SetTextInputRect(&rect);
+    }
 }
 
 /// https://wiki.libsdl.org/SDL_TextEditingEvent
