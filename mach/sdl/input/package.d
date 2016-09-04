dece35900f9558e8ce7c281241c371c4d2a00e31
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-03 20:50:23 -0400

    Added package.d files to mach.sdl.input and mach.sdl.input.event

---
+++ mach/sdl/input/package.d
@@ -0,0 +1,13 @@
+module mach.sdl.input;
+
+public:
+
+import mach.sdl.input.event;
+
+import mach.sdl.input.common;
+import mach.sdl.input.controller;
+import mach.sdl.input.joystick;
+import mach.sdl.input.keyboard;
+import mach.sdl.input.keycode;
+import mach.sdl.input.keymod;
+import mach.sdl.input.mouse;
