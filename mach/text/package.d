f4edd8805ac301dc748e3df8263e3d63dd278149
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-16 20:55:48 -0400

    Added package.d to mach.text.parse

--- mach/text/package.d
+++ mach/text/package.d
@@ -5,6 +5,7 @@ public:
 import mach.text.cases;
 import mach.text.english;
 import mach.text.escape : Escaper;
+import mach.text.parse;
 import mach.text.text : text;
 import mach.text.wildcard : WildMatcher;
 import mach.text.utf : utfdecode;
