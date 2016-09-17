3d71e9b7c6a12fbb414af5eda82d26000ac7a490
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-16 20:46:43 -0400

    mach.range.join now allows specifying whether to begin/end with the separator

--- mach/range/join.d
+++ mach/range/join.d
@@ -21,18 +21,16 @@ template canJoin(Iter, Sep){
 
 
 /// Archetypical string join operation. ["abc", "def"].join(", ") == "abc, def"
-auto join(Iter, Sep)(auto ref Iter iter, auto ref Sep separator) if(canJoin!(Iter, Sep)){
-    static if(canIntersperse!(Iter, Sep, size_t)){
-        alias intersep = separator;
-    }else{
-        Sep[1] intersep = [separator];
-    }
-    return chainiter(intersperse!(false, false)(iter, intersep, size_t(2)));
+auto join(bool frontsep = false, bool backsep = false, Iter, Sep)(
+    auto ref Iter iter, auto ref Sep separator
+) if(canJoin!(Iter, Sep)){
+    return chainiter(intersperse!(frontsep, backsep)(iter, separator, size_t(2)));
 }
 
 /// Special case intended to allow joining an array of strings using a character.
-auto join(T)(T[][] strings, T ch){
-    return join(strings, [ch]);
+/// TODO: Abstract this (Probably easier said than done)
+auto join(bool frontsep = false, bool backsep = false, T)(T[][] strings, T ch){
+    return join!(frontsep, backsep)(strings, [ch]);
 }
 
 /// In the absence of a separator just chain the input iterator. (Because why not?)
@@ -53,6 +51,9 @@ unittest{
         test(["abc", "def", "ghi"].join("").equals("abcdefghi"));
         test(["abc", "def", "ghi"].join.equals("abcdefghi"));
         test(["abc", "def", "ghi"].join(',').equals("abc,def,ghi"));
+        test(["a", "b"].join!(false, true)('.').equals("a.b."));
+        test(["a", "b"].join!(true, false)('.').equals(".a.b"));
+        test(["a", "b"].join!(true, true)('.').equals(".a.b."));
         test(["abc"].join(", ").equals("abc"));
         test(["abc"].join("").equals("abc"));
         test((new string[0]).join(", ").equals(""));
