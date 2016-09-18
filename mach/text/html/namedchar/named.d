b42fb68cc880f1e4d9d2fb936517dc74fcaf6a19
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-18 13:32:04 -0400

    Added unittests from readme to namedchar package

--- mach/text/html/namedchar/named.d
+++ mach/text/html/namedchar/named.d
@@ -113,5 +113,11 @@ unittest{
             testeq(NamedChar.getpoints("GT"), ">"d);
             testeq(NamedChar.getpoints("acE"), "\u223E\u0333"d);
         });
+        tests("In readme", {
+            assert(NamedChar.isnamed("á"));
+            assert(NamedChar.getname("á") == "aacute");
+            assert(NamedChar.isname("aacute"));
+            assert(NamedChar.getpoints("aacute") == "á"d);
+        });
     });
 }
