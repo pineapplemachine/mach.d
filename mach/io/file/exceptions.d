355b2bd611ee94b71178ab9491712c2a0a1525c4
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-24 19:12:36 -0400

    Improved mach.io.file.exceptions

--- mach/io/file/exceptions.d
+++ mach/io/file/exceptions.d
@@ -15,42 +15,64 @@ class FileException: Exception{
     }
 }
 
-class FileStatException: FileException{
+
+
+alias FileStatException = FilePathExceptionTemplate!"Failed to stat file";
+alias FileSyncException = FilePathExceptionTemplate!"Failed to sync file";
+alias FileOpenException = FilePathExceptionTemplate!"Failed to open file";
+alias FileRemoveException = FilePathExceptionTemplate!"Failed to remove file";
+alias FileRenameException = FileSrcDstExceptionTemplate!"Failed to rename file";
+alias FileCopyException = FileSrcDstExceptionTemplate!"Failed to copy file";
+
+alias FileSetPermissionsException = FilePathExceptionTemplate!"Failed to set file permissions";
+alias FileSetTimeException = FilePathExceptionTemplate!"Failed to set file time information";
+
+alias FileChangeDirException = FilePathExceptionTemplate!"Failed to change current directory";
+alias FileMakeDirException = FilePathExceptionTemplate!"Failed to create directory";
+alias FileRemoveDirException = FilePathExceptionTemplate!"Failed to remove directory";
+
+class FileGetCurrentDirException: FileException{
     this(Throwable next = null, size_t line = __LINE__, string file = __FILE__){
-        super("Failed to stat file.", next, line, file);
+        super(text("Failed to get current directory."), next, line, file);
     }
 }
 
-class FileSyncException: FileException{
-    int error;
-    this(Err)(Err error, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
-        super(text("Failed to sync file, error code ", error, "."), next, line, file);
-        this.error = cast(int) error;
+
+
+private template FilePathExceptionTemplate(string message){
+    class FilePathExceptionTemplate: FileException{
+        string path;
+        
+        this(Throwable next = null, size_t line = __LINE__, string file = __FILE__){
+            super(text(message, "."), next, line, file);
+            this.path = path;
+        }
+        this(string path, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
+            super(text(message, " \"", path, "\"."), next, line, file);
+            this.path = path;
+        }
+        this(string info, string path, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
+            super(text(message, " \"", path, "\": ", info), next, line, file);
+            this.path = path;
+        }
     }
 }
 
-class FileOpenException: FileException{
-    string path;
-    this(string path, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
-        super(text("Failed to open file \"", path, "\"."), next, line, file);
-        this.path = path;
-    }
-}
-
-class FileRenameException: FileException{
-    string src;
-    string dst;
-    this(string src, string dst, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
-        super(text("Failed to rename file \"", src, "\" to \"", dst, "\"."), next, line, file);
-        this.src = src;
-        this.dst = dst;
-    }
-}
-
-class FileRemoveException: FileException{
-    string path;
-    this(string path, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
-        super(text("Failed to remove file \"", path, "\"."), next, line, file);
-        this.path = path;
+private template FileSrcDstExceptionTemplate(string message){
+    class FileSrcDstExceptionTemplate: FileException{
+        string src, dst;
+        
+        this(Throwable next = null, size_t line = __LINE__, string file = __FILE__){
+            super(text(message, "."), next, line, file);
+            this.src = src; this.dst = dst;
+        }
+        this(string src, string dst, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
+            super(text(message, " \"", src, "\" to \"", dst, "\"."), next, line, file);
+            this.src = src; this.dst = dst;
+        }
+        this(string info, string src, string dst, Throwable next = null, size_t line = __LINE__, string file = __FILE__){
+            super(text(message, " \"", src, "\" to \"", dst, "\": ", info), next, line, file);
+            this.src = src; this.dst = dst;
+        }
     }
 }
