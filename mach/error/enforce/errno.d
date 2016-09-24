725e7836d2ac6be6135194f18a1513e163cf540c
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-24 14:17:24 -0400

    Cleaner mach.error.enforce.errno
    Removed asserterrno function because I'm evil and I can

--- mach/error/enforce/errno.d
+++ mach/error/enforce/errno.d
@@ -4,7 +4,7 @@ private:
 
 import core.stdc.errno : errno;
 import core.stdc.string : strlen;
-import std.format : format;
+import mach.text : text;
 
 public:
 
@@ -15,29 +15,31 @@ alias Errno = typeof(errno());
 
 
 class ErrnoException: Exception{
-    Errno errno;
+    Errno error;
+    
     this(string message = null, size_t line = __LINE__, string file = __FILE__) @trusted{
-        this(message, .errno, line, file);
+        this(message, errno, line, file);
     }
-    this(string message, Errno errno, size_t line = __LINE__, string file = __FILE__) @trusted{
-        this.errno = errno;
-        super(geterrnostring(errno, message), file, line, null);
+    this(string message, Errno error, size_t line = __LINE__, string file = __FILE__) @trusted{
+        this.error = error;
+        super(geterrorstring(error, message), file, line, null);
     }
-    /// Builds an exception message string given an errno and additional text data.
-    static string geterrnostring(Errno errno, string message = null){
+    
+    /// Build an exception message string given an errno and additional text data.
+    static string geterrorstring(Errno error, string message = null){
         version(CRuntime_Glibc){
             import core.stdc.string : strerror_r;
             char[1024] buf = void;
-            auto errcstring = strerror_r(errno, buf.ptr, buf.length);
+            auto errcstring = strerror_r(error, buf.ptr, buf.length);
         }else{
             import core.stdc.string : strerror;
-            auto errcstring = strerror(errno);
+            auto errcstring = strerror(error);
         }
-        auto errstring = errcstring[0..errcstring.strlen].idup;
+        auto errstring = errcstring[0 .. errcstring.strlen].idup;
         if(message !is null){
-            return "%s (Errno %s: %s)".format(message, errno, errstring);
+            return text(message, " (Error code ", error, ": ", errstring, ")");
         }else{
-            return "Errno %s: %s".format(errno, errstring);
+            return text("Error code ", error, ": ", errstring);
         }
     }
 }
@@ -54,44 +56,24 @@ auto enforceerrno(T)(
     return condition;
 }
 
-/// If a condition is not met, throws an ErrnoException, but only when the code
-/// has been compiled with flags that cause assert statements to be evaluated.
-auto asserterrno(T)(
-    lazy T condition, string message = null,
-    size_t line = __LINE__, string file = __FILE__
-){
-    assert({
-        if(!condition()) throw new ErrnoException(message, line, file);
-        return true;
-    }());
-}
-
 
 
 version(unittest){
     private:
-    import mach.error.unit;
-    void TestErrno(alias errnofunc)(){
-        errno = 0;
-        errnofunc(true);
-        fail(
-            (e) => ((cast(ErrnoException) e).errno == 0),
-            {errnofunc(false);}
-        );
-        errno = 1;
-        fail(
-            (e) => ((cast(ErrnoException) e).errno == 1),
-            {errnofunc(false);}
-        );
-    }
+    import mach.test;
 }
 unittest{
     tests("Errno", {
-        tests("Enforce", {
-            TestErrno!enforceerrno;
-        });
-        tests("Assert", {
-            TestErrno!asserterrno;
-        });
+        enforceerrno(true);
+        errno = 0;
+        testfail(
+            (Throwable e) => ((cast(ErrnoException) e).error == 0),
+            {enforceerrno(false);}
+        );
+        errno = 1;
+        testfail(
+            (Throwable e) => ((cast(ErrnoException) e).error == 1),
+            {enforceerrno(false);}
+        );
     });
 }
