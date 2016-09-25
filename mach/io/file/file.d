59b63f0e6562cbc638163380078680ca01c0c092
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-25 07:50:03 -0400

    Additions to mach.io.file.file

--- mach/io/file/file.d
+++ mach/io/file/file.d
@@ -2,7 +2,11 @@ module mach.io.file.file;
 
 private:
 
-import mach.io.stream.filestream : FileStream;
+import mach.traits : isIterable;
+import mach.io.stream : FileStream, asrange;
+import mach.io.file.exceptions;
+import mach.io.file.stat;
+import mach.io.file.sys;
 
 public:
 
@@ -14,6 +18,7 @@ abstract class File{
     static auto open(string path, in char[] mode = "rb"){
         return new FileStream(path, mode);
     }
+    
     /// Open the file at a path in read mode.
     static auto read(string path){
         return File.open(path, "rb");
@@ -26,8 +31,63 @@ abstract class File{
     static auto append(string path){
         return File.open(path, "ab");
     }
+    
     /// Create and open a new temporary file.
     static auto temp(){
         return FileStream.temp();
     }
+    
+    /// Remove a file.
+    static void rename(string src, string dst){
+        .rename(src, dst);
+    }
+    /// Remove the file or directory at a path.
+    static void remove(string path){
+        if(isfile(path)){
+            rmfile(path);
+        }else if(isdir(path)){
+            rmdir(path);
+        }else{
+            throw new FileRemoveException(path);
+        }
+    }
+    /// Get whether the path exists.
+    static auto exists(string path){
+        return .exists(path);
+    }
+    
+    /// Get whether a path points to a file.
+    static auto isfile(string path){
+        return .isfile(path);
+    }
+    /// Get whether a path points to a directory.
+    static auto isdir(string path){
+        return .isdir(path);
+    }
+    static auto islink(string path){
+        return .islink(path);
+    }
+    
+    /// Read data from a file path as a range with elements of type T.
+    static auto readfrom(T = char)(string path){
+        return read(path).asrange!T;
+    }
+    /// Write some data to a file path.
+    static auto writeto(T)(string path, T data) if(isIterable!T){
+        auto file = write(path);
+        scope(exit) file.close();
+        file.write(data);
+    }
+}
+
+
+
+version(unittest){
+    private:
+    import mach.test;
+}
+unittest{
+    tests("File", {
+        // TODO
+    });
 }
