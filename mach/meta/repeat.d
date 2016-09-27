8111aa4103fb941fcf0d56dd896cf19c58825a49
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-27 17:28:20 -0400

    Updated mach.meta.repeat to better handle edge case

--- mach/meta/repeat.d
+++ mach/meta/repeat.d
@@ -10,7 +10,7 @@ public:
 
 /// Repeat a list of aliases some given number of times.
 template Repeat(size_t count, T...){
-    static if(count == 0){
+    static if(count == 0 || T.length == 0){
         alias Repeat = Aliases!();
     }else static if(count == 1){
         alias Repeat = T;
@@ -22,6 +22,8 @@ template Repeat(size_t count, T...){
 
 
 unittest{
+    static assert(is(Repeat!(0) == Aliases!()));
+    static assert(is(Repeat!(1) == Aliases!()));
     static assert(is(Repeat!(0, int, long) == Aliases!()));
     static assert(is(Repeat!(1, int, long) == Aliases!(int, long)));
     static assert(is(Repeat!(2, int, long) == Aliases!(int, long, int, long)));
