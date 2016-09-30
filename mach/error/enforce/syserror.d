057b7735213c3db1ae754b50cb214b838f9d9803
Sophie Kirschner <pineapplemachine@gmail.com>
2016-09-30 16:34:29 -0400

    Fixed compile errors on non-windows platforms (hopefully)
    I should probably actually test this but I don't feel like getting my laptop out right now

--- mach/error/enforce/syserror.d
+++ mach/error/enforce/syserror.d
@@ -1,7 +1,5 @@
 module mach.error.enforce.syserror;
 
-version(Windows):
-
 private:
 
 import std.windows.syserror : GetLastError, sysErrorString;
@@ -11,60 +9,63 @@ public:
 
 
 
-alias SysError = typeof(GetLastError());
-
-
-
-class SysErrorException: Exception{
-    SysError error;
+version(Windows){
+    alias SysError = typeof(GetLastError());
     
-    this(string message = null, size_t line = __LINE__, string file = __FILE__) @trusted{
-        this(message, GetLastError(), line, file);
-    }
-    this(string message, SysError error, size_t line = __LINE__, string file = __FILE__) @trusted{
-        this.error = error;
-        super(geterrorstring(error, message), file, line, null);
-    }
-    
-    static string geterrorstring(SysError error, string message = null){
-        auto errstring = sysErrorString(error);
-        if(message !is null){
-            return text(message, " (Error code ", error, ": ", errstring, ")");
-        }else{
-            return text("Error code ", error, ": ", errstring);
+    class SysErrorException: Exception{
+        SysError error;
+        
+        this(string message = null, size_t line = __LINE__, string file = __FILE__) @trusted{
+            this(message, GetLastError(), line, file);
+        }
+        this(string message, SysError error, size_t line = __LINE__, string file = __FILE__) @trusted{
+            this.error = error;
+            super(geterrorstring(error, message), file, line, null);
+        }
+        
+        static string geterrorstring(SysError error, string message = null){
+            auto errstring = sysErrorString(error);
+            if(message !is null){
+                return text(message, " (Error code ", error, ": ", errstring, ")");
+            }else{
+                return text("Error code ", error, ": ", errstring);
+            }
         }
     }
-}
-
-
-
-auto enforcesyserror(T)(
-    T condition, string message = null,
-    size_t line = __LINE__, string file = __FILE__
-){
-    if(!condition) throw new SysErrorException(message, line, file);
-    return condition;
+    
+    auto enforcesyserror(T)(
+        T condition, string message = null,
+        size_t line = __LINE__, string file = __FILE__
+    ){
+        if(!condition) throw new SysErrorException(message, line, file);
+        return condition;
+    }
+}else{
+    alias SysError = void;
+    
+    class SysErrorException: Exception{
+        this(Args...)(auto ref Args args){
+            assert(false, "Operation only meaningful on Windows platforms.");
+        }
+    }
+    
+    auto enforcesyserror(T)(
+        T condition, string message = null,
+        size_t line = __LINE__, string file = __FILE__
+    ){
+        assert(false, "Operation only meaningful on Windows platforms.");
+    }
 }
 
 
 
 version(unittest){
     private:
-    import mach.error.unit;
+    import mach.test;
 }
 unittest{
     tests("Windows error", {
-        //enforcesyserror(true);
-        //errno = 0;
-        //fail(
-        //    (e) => ((cast(ErrnoException) e).errno == 0),
-        //    {enforceerrno(false);}
-        //);
-        //errno = 1;
-        //fail(
-        //    (e) => ((cast(ErrnoException) e).errno == 1),
-        //    {enforceerrno(false);}
-        //);
+        // TODO
     });
 }
 
