ecd7a4703de4193cd2864a9acaff0f1f7123768e
Sophie Kirschner <pineapplemachine@gmail.com>
2016-10-01 18:37:51 -0400

    Cleanup in mach.range.pad

--- mach/range/pad.d
+++ mach/range/pad.d
@@ -3,9 +3,8 @@ module mach.range.pad;
 private:
 
 import std.conv : to;
-import std.traits : isIntegral, isImplicitlyConvertible;
 import mach.traits : isBidirectionalRange, isRandomAccessRange, isSlicingRange;
-import mach.traits : isRange, ElementType, hasNumericLength;
+import mach.traits : isRange, ElementType, hasNumericLength, isIntegral;
 import mach.range.asrange : asrange, validAsRange;
 import mach.range.meta : MetaRangeMixin;
 
@@ -13,127 +12,139 @@ public:
 
 
 
-enum canPad(Iter, Count) = (
-    validAsRange!Iter && validPadCount!Count
+enum canPad(Iter) = (
+    validAsRange!Iter
 );
 
-enum canPad(Iter, Element, Count) = (
-    canPad!(Iter, Count) && isImplicitlyConvertible!(Element, ElementType!Iter)
+enum canPad(Iter, Element) = (
+    canPad!(Iter) && is(typeof({
+        ElementType!Iter i = Element.init;
+    }))
 );
 
-enum canPadLength(Iter, Count) = (
-    validAsRange!Iter && hasNumericLength!Iter && validPadCount!Count
+enum canPadLength(Iter) = (
+    validAsRange!Iter && hasNumericLength!Iter
 );
 
-enum canPadLength(Iter, Element, Count) = (
-    canPadLength!(Iter, Count) && isImplicitlyConvertible!(Element, ElementType!Iter)
+enum canPadLength(Iter, Element) = (
+    canPadLength!(Iter) && is(typeof({
+        ElementType!Iter i = Element.init;
+    }))
 );
 
-enum canPadRange(Range, Count) = (
-    isRange!Range && validPadCount!Count
+enum canPadRange(Range) = (
+    isRange!Range
 );
-
-alias validPadCount = isIntegral;
 
 
 
 /// Pad a range on both the left and right sides by a specified amount.
-auto pad(Iter, Element, Count = size_t)(
-    Iter iter, Element padding, Count leftright
-) if(canPad!(Iter, Element, Count)){
+auto pad(Iter, Element)(
+    auto ref Iter iter, auto ref Element padding, size_t leftright
+) if(canPad!(Iter, Element)){
     return pad(iter, padding, leftright, leftright);
 }
 /// ditto
-auto pad(Iter, Count = size_t)(
-    Iter iter, Count leftright
-) if(canPad!(Iter, Count)){
+auto pad(Iter)(
+    auto ref Iter iter, size_t leftright
+) if(canPad!(Iter)){
     return pad(iter, ElementType!Iter.init, leftright);
 }
 
+
+
 /// Pad a range on both the left and right sides by specified amounts.
-auto pad(Iter, Element, Count = size_t)(
-    Iter iter, Element padding, Count left, Count right
-) if(canPad!(Iter, Element, Count)){
+auto pad(Iter, Element)(
+    auto ref Iter iter, auto ref Element padding, size_t left, size_t right
+) if(canPad!(Iter, Element)){
     return pad(iter, padding, padding, left, right);
 }
 /// ditto
-auto pad(Iter, Count = size_t)(
-    Iter iter, Count left, Count right
-) if(canPad!(Iter, Count)){
+auto pad(Iter)(
+    auto ref Iter iter, size_t left, size_t right
+) if(canPad!(Iter)){
     return pad(iter, ElementType!Iter.init, left, right);
 }
 
+
+
 /// Pad a range on both the left and right sides by specified amounts.
-auto pad(Iter, Element, Count = size_t)(
-    Iter iter, Element paddingleft, Element paddingright, Count left, Count right
-) if(canPad!(Iter, Element, Count)){
+auto pad(Iter, Element)(
+    auto ref Iter iter, auto ref Element paddingleft, auto ref Element paddingright, size_t left, size_t right
+) if(canPad!(Iter, Element)){
     auto range = iter.asrange;
-    return PadRange!(typeof(range), Count)(
+    return PadRange!(typeof(range))(
         range, paddingleft, paddingright, left, right
     );
 }
 
+
+
 /// Pad a range on the left such that it is at least as long as the given length.
-auto padfront(Iter, Element, Count = size_t)(
-    Iter iter, Element padding, Count length
-) if(canPadLength!(Iter, Element, Count)){
+auto padfront(Iter, Element)(
+    auto ref Iter iter, auto ref Element padding, size_t length
+) if(canPadLength!(Iter, Element)){
     auto left = length <= iter.length ? 0 : length - iter.length;
     return padfrontcount(iter, padding, left);
 }
 /// ditto
-auto padfront(Iter, Count = size_t)(
-    Iter iter, Count length
-) if(canPadLength!(Iter, Count)){
+auto padfront(Iter)(
+    auto ref Iter iter, size_t length
+) if(canPadLength!(Iter)){
     return padfront(iter, ElementType!Iter.init, length);
 }
 
+
+
 /// Pad a range on the right such that it is at least as long as the given length.
-auto padback(Iter, Element, Count = size_t)(
-    Iter iter, Element padding, Count length
-) if(canPadLength!(Iter, Element, Count)){
+auto padback(Iter, Element)(
+    auto ref Iter iter, auto ref Element padding, size_t length
+) if(canPadLength!(Iter, Element)){
     auto right = length <= iter.length ? 0 : length - iter.length;
     return padbackcount(iter, padding, right);
 }
 /// ditto
-auto padback(Iter, Count = size_t)(
-    Iter iter, Count length
-) if(canPadLength!(Iter, Count)){
+auto padback(Iter)(
+    auto ref Iter iter, size_t length
+) if(canPadLength!(Iter)){
     return padback(iter, ElementType!Iter.init, length);
 }
 
 
+
 /// Pad a range on the left by the specified amount.
-auto padfrontcount(Iter, Element, Count = size_t)(
-    Iter iter, Element padding, Count left
-) if(canPad!(Iter, Element, Count)){
+auto padfrontcount(Iter, Element)(
+    auto ref Iter iter, auto ref Element padding, size_t left
+) if(canPad!(Iter, Element)){
     auto range = iter.asrange;
-    return PadRange!(typeof(range), Count)(range, padding, left, Count.init);
+    return PadRange!(typeof(range))(range, padding, left, 0);
 }
 /// ditto
-auto padfrontcount(Iter, Count = size_t)(
-    Iter iter, Count left
-) if(canPadLength!(Iter, Count)){
+auto padfrontcount(Iter)(
+    auto ref Iter iter, size_t left
+) if(canPadLength!(Iter)){
     return padfrontcount(iter, ElementType!Iter.init, left);
 }
 
 
+
 /// Pad a range on the right by the specified amount.
-auto padbackcount(Iter, Element, Count = size_t)(
-    Iter iter, Element padding, Count right
-) if(canPad!(Iter, Element, Count)){
+auto padbackcount(Iter, Element)(
+    auto ref Iter iter, auto ref Element padding, size_t right
+) if(canPad!(Iter, Element)){
     auto range = iter.asrange;
-    return PadRange!(typeof(range), Count)(range, padding, Count.init, right);
+    return PadRange!(typeof(range))(range, padding, 0, right);
 }
 /// ditto
-auto padbackcount(Iter, Count = size_t)(
-    Iter iter, Count right
-) if(canPadLength!(Iter, Count)){
+auto padbackcount(Iter)(
+    auto ref Iter iter, size_t right
+) if(canPadLength!(Iter)){
     return padbackcount(iter, ElementType!Iter.init, right);
 }
 
 
 
-struct PadRange(Range, Count = size_t) if(canPadRange!(Range, Count)){
+struct PadRange(Range) if(canPadRange!(Range)){
     alias Element = ElementType!Range;
     
     mixin MetaRangeMixin!(
@@ -143,18 +154,18 @@ struct PadRange(Range, Count = size_t) if(canPadRange!(Range, Count)){
     Range source; /// Range to pad
     Element frontpadding; /// Element to use as padding to the left
     Element backpadding; /// Element to use as padding to the right
-    Count frontcount; /// Number of padding elements enumerated so far in front
-    Count backcount; /// Number of padding elements enumerated so far in back
-    Count frontmax; /// Number of padding elements to enumerate in total to the range's left
-    Count backmax; /// Number of padding elements to enumerate in total to the range's right
+    size_t frontcount; /// Number of padding elements enumerated so far in front
+    size_t backcount; /// Number of padding elements enumerated so far in back
+    size_t frontmax; /// Number of padding elements to enumerate in total to the range's left
+    size_t backmax; /// Number of padding elements to enumerate in total to the range's right
     
-    this(Range source, Element padding, Count frontmax, Count backmax){
+    this(Range source, Element padding, size_t frontmax, size_t backmax){
         this(source, padding, padding, frontmax, backmax);
     }
     this(
         Range source, Element frontpadding, Element backpadding,
-        Count frontmax, Count backmax,
-        Count frontcount = Count.init, Count backcount = Count.init
+        size_t frontmax, size_t backmax,
+        size_t frontcount = 0, size_t backcount = 0
     ){
         this.source = source;
         this.frontpadding = frontpadding;
@@ -264,7 +275,8 @@ struct PadRange(Range, Count = size_t) if(canPadRange!(Range, Count)){
                 return typeof(this)(
                     this.source[sourcelow .. sourcehigh],
                     this.frontpadding, this.backpadding,
-                    to!Count(slicepadfront), to!Count(slicepadback)
+                    cast(size_t) slicepadfront,
+                    cast(size_t) slicepadback
                 );
             }
         }
@@ -275,7 +287,7 @@ struct PadRange(Range, Count = size_t) if(canPadRange!(Range, Count)){
 
 version(unittest){
     private:
-    import mach.error.unit;
+    import mach.test;
     import mach.range.compare : equals;
 }
 unittest{
@@ -283,8 +295,8 @@ unittest{
         auto input = "hi";
         tests("Front", {
             auto range = input.padfront('_', 4);
-            testeq("Length", range.length, 4);
-            test("Iteration", range.equals("__hi"));
+            testeq(range.length, 4);
+            test!equals(range, "__hi");
             tests("Random access", {
                 testeq(range[0], '_');
                 testeq(range[1], '_');
@@ -294,8 +306,8 @@ unittest{
         });
         tests("Back", {
             auto range = input.padback('_', 4);
-            testeq("Length", range.length, 4);
-            test("Iteration", range.equals("hi__"));
+            testeq(range.length, 4);
+            test!equals(range, "hi__");
             tests("Random access", {
                 testeq(range[0], 'h');
                 testeq(range[1], 'i');
@@ -305,8 +317,8 @@ unittest{
         });
         tests("Both", {
             auto range = input.pad('F', 'B', 2, 3);
-            testeq("Length", range.length, 7);
-            test("Iteration", range.equals("FFhiBBB"));
+            testeq(range.length, 7);
+            test!equals(range, "FFhiBBB");
             tests("Random access", {
                 testeq(range[0], 'F');
                 testeq(range[1], 'F');
