6862e82b9ea1602c9df5da8721a507ed33cef413
Sophie Kirschner <pineapplemachine@gmail.com>
2016-10-10 14:25:00 -0400

    Use Rebindable in mach.range.top to make functions work on const inputs
    Previously the top and bottom functions were liable to just break if the elements of the input weren't of a rebindable type

--- mach/range/top.d
+++ mach/range/top.d
@@ -3,6 +3,7 @@ module mach.range.top;
 private:
 
 import std.functional : not;
+import mach.types : Rebindable;
 import mach.traits : ElementType, isFiniteIterable, isPredicate;
 import mach.collect : SortedList;
 
@@ -30,14 +31,14 @@ template top(alias compare = DefaultTopComparison){
     ) if(canTop!(Iter, compare)){
         alias Element = ElementType!Iter;
         bool first = true;
-        Element top;
+        Rebindable!Element top;
         foreach(element; iter){
             if(first || compare(element, top)){
                 top = element;
                 first = false;
             }
         }
-        return top;
+        return cast(Element) top;
     }
     auto top(Iter)(
         auto ref Iter iter, size_t count
@@ -68,19 +69,25 @@ template bottom(alias compare = DefaultTopComparison){
 
 version(unittest){
     private:
-    import mach.error.unit;
+    import mach.test;
     import mach.range.compare : equals;
 }
 unittest{
     tests("Top and bottom", {
-        auto input = [3, 2, 4, 1, 5];
         tests("Top", {
+            auto input = [3, 2, 4, 1, 5];
             testeq(input.top, 5);
-            test(input.top(3).equals([5, 4, 3]));
+            test!equals(input.top(3), [5, 4, 3]);
         });
         tests("Bottom", {
+            auto input = [3, 2, 4, 1, 5];
             testeq(input.bottom, 1);
-            test(input.bottom(3).equals([1, 2, 3]));
+            test!equals(input.bottom(3), [1, 2, 3]);
         });
+        tests("Const input", {
+            const(const(int)[]) input = [0, 1, 2, 3];
+            testeq(input.top, 3);
+            test!equals(input.top(2), [3, 2]);
+        }); 
     });
 }
