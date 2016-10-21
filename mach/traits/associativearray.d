e454de0f55b47ef58b80a82c2f74676aa912e0f1
Sophie Kirschner <pineapplemachine@gmail.com>
2016-10-21 16:34:09 -0400

    Made mach.traits.associativearray less bad

--- mach/traits/associativearray.d
+++ mach/traits/associativearray.d
@@ -9,8 +9,6 @@ public:
 
 
 /// Determine if a type is an associative array.
-enum isAssociativeArray(alias T) = isAssociativeArray!(typeof(T));
-/// ditto
 template isAssociativeArray(T){
     enum bool isAssociativeArray = is(typeof({
         auto x(K, V)(K[V] array){}
@@ -21,15 +19,11 @@ template isAssociativeArray(T){
 
 
 /// Get the key type for an associative array.
-enum ArrayKeyType(alias T) = ArrayKeyType!(typeof(T));
-/// ditto
 template ArrayKeyType(T) if(isAssociativeArray!T){
     alias ArrayKeyType = typeof(T.init.keys[0]);
 }
 
 /// Get the value type for an associative array.
-enum ArrayValueType(alias T) = ArrayValueType!(typeof(T));
-/// ditto
 template ArrayValueType(T) if(isAssociativeArray!T){
     alias ArrayValueType = typeof(T.init.values[0]);
 }
@@ -37,13 +31,10 @@ template ArrayValueType(T) if(isAssociativeArray!T){
 
 
 unittest{
-    int[int] aa;
-    static assert(isAssociativeArray!(aa));
     static assert(isAssociativeArray!(int[int]));
     static assert(isAssociativeArray!(char[int]));
     static assert(isAssociativeArray!(const(int)[const(int)]));
     static assert(isAssociativeArray!(int[][string]));
-    static assert(!isAssociativeArray!(null));
     static assert(!isAssociativeArray!(void));
     static assert(!isAssociativeArray!(int));
     static assert(!isAssociativeArray!(int[]));
