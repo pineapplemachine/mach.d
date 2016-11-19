162653326a22461ddb7800bfceeb8fb832207d8c
Sophie Kirschner <pineapplemachine@gmail.com>
2016-11-19 09:54:04 -0500

    Pruned parts of WIP ini parsing package that could cause compile issues
    Will recommit later, if I ever get around to finishing this thing

--- mach/text/parse/ini/package.d
+++ mach/text/parse/ini/package.d
@@ -2,7 +2,7 @@ module mach.text.parse.ini;
 
 public:
 
-import mach.text.parse.ini.parse : IniFile;
+//import mach.text.parse.ini.parse : IniFile;
 import mach.text.parse.ini.exceptions : IniException, IniParseException;
 import mach.text.parse.ini.properties : IniProperty, IniProperties;
-import mach.text.parse.ini.settings : IniSettings;
+//import mach.text.parse.ini.settings : IniSettings;
