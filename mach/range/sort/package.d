8b59313e151a6dd5b83e68d7b9e3b48d5dcbcaf8
Sophie Kirschner <pineapplemachine@gmail.com>
2016-11-22 20:44:46 -0500

    Fixed selectionsort imports in mach.range.sort package.d

--- mach/range/sort/package.d
+++ mach/range/sort/package.d
@@ -5,5 +5,5 @@ public:
 import mach.range.sort.issorted : issorted;
 import mach.range.sort.insertionsort : insertionsort, linearinsertionsort, binaryinsertionsort;
 import mach.range.sort.mergesort : mergesort;
-import mach.range.sort.selectionsort : selectionsort;
+import mach.range.sort.selectionsort : selectionsort, lazyselectionsort, eagerselectionsort;
 import mach.range.sort.shellsort : shellsort;
