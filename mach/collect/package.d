5993b40fd5f5ec20a928687e80a35e828ac5a8b2
Sophie Kirschner <pineapplemachine@gmail.com>
2016-12-02 16:27:51 -0500

    Removed mach.collect.orderedaa entirely
    At some point I'll rewrite this and make it not broken

--- mach/collect/package.d
+++ mach/collect/package.d
@@ -4,6 +4,5 @@ public:
 
 import mach.collect.heap : Heap, heapify;
 import mach.collect.linkedlist : LinkedList, aslist;
-//import mach.collect.orderedaa : OrderedAA; // TODO: Why does this break?
 import mach.collect.set;
 import mach.collect.sortedlist : SortedList;
