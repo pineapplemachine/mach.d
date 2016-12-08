253805958813263c639eabf5d959c66474b78329
Sophie Kirschner <pineapplemachine@gmail.com>
2016-12-08 15:31:38 -0500

    Fixes, additions, and better tests in mach.range.random.templates

--- mach/range/random/templates.d
+++ mach/range/random/templates.d
@@ -16,14 +16,16 @@ enum isRNG(T) = (
 
 template RNGMixin(T){
     import mach.traits : isIntegral, isFloatingPoint, isNumeric, isUnsigned;
+    import mach.traits : isEnumType, getEnumLength, getenummember;
     
     static enum bool rng = true;
     
     // This arithmetic is hard if T is signed so please don't make T signed
     static if(isNumeric!T && isUnsigned!T){
         /// Get the front of an RNG as a floating point number. Should be unbiased.
+        /// The range is inclusive.
         As random(As)(in As low, in As high) if(isFloatingPoint!As){
-            return this.random!As(high) + low;
+            return this.random!As(high - low) + low;
         }
         
         /// ditto
@@ -46,7 +48,7 @@ template RNGMixin(T){
             assert(high - low <= T.max);
         }body{
             scope(exit) this.popFront();
-            return low + this.front % (high - low);
+            return cast(As)(low + this.front % (high - low));
         }
         
         /// ditto
@@ -64,6 +66,14 @@ template RNGMixin(T){
             scope(exit) this.popFront();
             return cast(As) this.front; // Assumes overflow wraps around
         }
+        
+        /// Get the front of an RNG as a member of an enum.
+        /// Not guaranteed to be unbiased, but should be close enough for almost
+        /// any use case.
+        As random(As)() if(isEnumType!As){
+            scope(exit) this.popFront();
+            return getenummember!As(this.random!size_t(getEnumLength!As));
+        }
     }
 }
 
@@ -72,9 +82,11 @@ template RNGMixin(T){
 version(unittest){
     private:
     import mach.test;
+    import mach.meta : Aliases;
     import mach.range.random.lcong : lcong;
     import mach.range.random.mersenne : mersenne;
     import mach.range.random.xorshift : xorshift;
+    enum Enum{A, B, C}
 }
 unittest{
     // TODO
@@ -83,5 +95,19 @@ unittest{
     static assert(isRNG!(typeof(xorshift())));
     static assert(!isRNG!int);
     static assert(!isRNG!(int[]));
+    foreach(RNG; Aliases!(lcong, mersenne, xorshift)){
+        auto rng = RNG();
+        Enum e = rng.random!Enum; // Enum
+        foreach(test; 0 .. 20){
+            foreach(low; [1, 0, -1, -2, -3, -4]){
+                foreach(high; [2, 3, 4, 5, 6, 7, 8, 9]){
+                    auto i = rng.random!int(low, high); // Signed int
+                    assert(i >= low && i < high);
+                    auto f = rng.random!double(low, high); // Float
+                    assert(f >= low && f <= high);
+                }
+            }
+        }
+    }
 }
 
