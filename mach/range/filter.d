50463a624a8f3bd0695d44683a86dbe979ccf14d
Sophie Kirschner <pineapplemachine@gmail.com>
2016-12-09 15:10:03 -0500

    Cleanup & improvements in mach.range.filter
    - Much more thorough tests
    - Added removeFront and removeBack
    - Better template constraints
    - Stop using MetaRangeMixin

--- mach/range/filter.d
+++ mach/range/filter.d
@@ -2,21 +2,25 @@ module mach.range.filter;
 
 private:
 
-import mach.traits : ElementType, isRange, isBidirectionalRange, isElementPredicate;
+import mach.traits : ElementType, isRange, isBidirectionalRange, isSavingRange;
 import mach.traits : isMutableRange, isMutableFrontRange, isMutableBackRange;
-import mach.range.asrange : asrange, validAsRange;
-import mach.range.meta : MetaRangeMixin;
+import mach.traits : isMutableRemoveFrontRange, isMutableRemoveBackRange;
+import mach.range.asrange : asrange, validAsRange, AsRangeElementType;
+import mach.range.meta : MetaRangeEmptyMixin;
 
 public:
 
 
 
-enum canFilter(Iter, alias pred) = (
-    validAsRange!Iter && isElementPredicate!(pred, Iter)
-);
-enum canFilterRange(Range, alias pred) = (
-    isRange!Range && isElementPredicate!(pred, Range)
-);
+template canFilter(T, alias pred){
+    enum bool canFilter = validAsRange!T && is(typeof({
+        if(pred(AsRangeElementType!T.init)){}
+    }));
+}
+
+template canFilterRange(T, alias pred){
+    enum bool canFilterRange = isRange!T && canFilter!(T, pred);
+}
 
 
 
@@ -32,9 +36,7 @@ auto filter(alias pred, Iter)(Iter iter) if(canFilter!(Iter, pred)){
 struct FilterRange(alias pred, Range) if(canFilterRange!(Range, pred)){
     alias Element = typeof(Range.front);
     
-    mixin MetaRangeMixin!(
-        Range, `source`, `Empty Save`
-    );
+    mixin MetaRangeEmptyMixin!Range;
     
     Range source;
     
@@ -44,10 +46,10 @@ struct FilterRange(alias pred, Range) if(canFilterRange!(Range, pred)){
         static if(isBidirectionalRange!Range) this.consumeBack();
     }
     
-    @property auto ref front(){
+    @property auto front() in{assert(!this.empty);} body{
         return this.source.front;
     }
-    void popFront(){
+    void popFront() in{assert(!this.empty);} body{
         this.source.popFront();
         this.consumeFront();
     }
@@ -59,10 +61,10 @@ struct FilterRange(alias pred, Range) if(canFilterRange!(Range, pred)){
     }
     
     static if(isBidirectionalRange!Range){
-        @property auto ref back(){
+        @property auto back() in{assert(!this.empty);} body{
             return this.source.back;
         }
-        void popBack(){
+        void popBack() in{assert(!this.empty);} body{
             this.source.popBack();
             this.consumeBack();
         }
@@ -76,17 +78,35 @@ struct FilterRange(alias pred, Range) if(canFilterRange!(Range, pred)){
     static if(isMutableRange!Range){
         enum bool mutable = true;
         static if(isMutableFrontRange!Range){
-            @property void front(Element value){
+            @property void front(Element value) in{assert(!this.empty);} body{
                 this.source.front = value;
             }
         }
         static if(isMutableBackRange!Range){
-            @property void back(Element value){
+            @property void back(Element value) in{assert(!this.empty);} body{
                 this.source.back = value;
+            }
+        }
+        static if(isMutableRemoveFrontRange!Range){
+            auto removeFront(){
+                scope(exit) this.consumeFront();
+                return this.source.removeFront();
+            }
+        }
+        static if(isMutableRemoveBackRange!Range){
+            auto removeBack(){
+                scope(exit) this.consumeBack();
+                return this.source.removeBack();
             }
         }
     }else{
         enum bool mutable = false;
+    }
+    
+    static if(isSavingRange!Range){
+        @property typeof(this) save(){
+            return typeof(this)(this.source.save);
+        }
     }
 }
 
@@ -99,20 +119,70 @@ version(unittest){
     import mach.range.consume : consume;
     import mach.range.mutate : mutate;
     import mach.range.retro : retro;
+    import mach.collect : DoublyLinkedList;
 }
 unittest{
     tests("Filter", {
         alias even = (n) => (n % 2 == 0);
         tests("Iteration", {
+            auto empty = new int[0];
+            test(empty.filter!even.equals(empty));
+            test([0].filter!even.equals([0]));
+            test([1].filter!even.equals(empty));
             test([1, 2, 3, 4, 5, 6].filter!even.equals([2, 4, 6]));
         });
-        tests("Backwards", {
+        tests("Bidirectionality", {
             test([2, 4, 5].filter!even.retro.equals([4, 2]));
+            auto range = [0, 1, 2, 3, 4].filter!even;
+            testeq(range.back, 4);
+            range.popBack();
+            testeq(range.back, 2);
+            range.popBack();
+            testeq(range.back, 0);
+            range.popBack();
+            test(range.empty);
+            testfail({range.front;});
+            testfail({range.popFront();});
+            testfail({range.back;});
+            testfail({range.popBack();});
+        });
+        tests("Saving", {
+            auto range = [0, 1, 2, 3].filter!even;
+            auto saved = range.save;
+            range.popFront();
+            testeq(range.front, 2);
+            testeq(saved.front, 0);
         });
         tests("Mutability", {
-            auto array = [1, 2, 3, 4, 5, 6];
-            array.filter!even.mutate!((n) => (n - 1)).consume;
-            testeq(array, [1, 1, 3, 3, 5, 5]);
+            {
+                auto array = [1, 2, 3, 4, 5, 6];
+                array.filter!even.mutate!((n) => (n - 1)).consume;
+                testeq(array, [1, 1, 3, 3, 5, 5]);
+            }{
+                auto list = new DoublyLinkedList!int(0, 1, 2, 3, 4, 5);
+                auto range = list.filter!(n => n % 2);
+                testeq(range.front, 1);
+                testeq(range.back, 5);
+                range.front = 6;
+                testeq(range.front, 6);
+                test!equals(list.ivalues, [0, 6, 2, 3, 4, 5]);
+                range.back = 7;
+                testeq(range.back, 7);
+                test!equals(list.ivalues, [0, 6, 2, 3, 4, 7]);
+                range.removeFront();
+                testeq(range.front, 3);
+                test!equals(list.ivalues, [0, 2, 3, 4, 7]);
+                range.removeBack();
+                testeq(range.back, 3);
+                test!equals(list.ivalues, [0, 2, 3, 4]);
+                range.removeFront();
+                test(range.empty);
+                test!equals(list.ivalues, [0, 2, 4]);
+                testfail({range.front = 0;});
+                testfail({range.back = 0;});
+                testfail({range.removeFront();});
+                testfail({range.removeBack();});
+            }
         });
     });
 }
