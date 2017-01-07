77dc08788c63dc826d03162e94d31442b8c1265f
Sophie Kirschner <pineapplemachine@gmail.com>
2017-01-07 18:31:06 -0500

    Fixes, more docs, and more tests in mach.range.contains
    Previously, searching for a substring with an explicitly-provided comparison function was not working correctly.

--- mach/range/contains.d
+++ mach/range/contains.d
@@ -4,6 +4,42 @@ private:
 
 import mach.range.find : find, DefaultFindIndex, canFindElementEager, canFindIterable;
 
+/++ Docs
+
+This module implements a `contains` function capable of searching an input
+iterable for an element satisfying a predicate, for an element equal to an
+input, or for a substring.
+
++/
+
+unittest{ /// Example
+    // Search for an equivalent element.
+    assert("hello".contains('h'));
+    assert(!"hello".contains('x'));
+}
+
+unittest{ /// Example
+    // Search for an element satisfying a predicate.
+    import mach.text.ascii : isupper;
+    assert("upper CASE".contains!isupper);
+    assert(!"lower case".contains!isupper);
+}
+
+unittest{ /// Example
+    // Search for a substring.
+    assert("hello world".contains("hello"));
+    assert("hello world".contains("world"));
+    assert(!"hello world".contains("nope"));
+}
+
+unittest{ /// Example
+    // Search for a case-insensitive substring.
+    import mach.text.ascii : tolower;
+    alias compare = (a, b) => (a.tolower == b.tolower);
+    assert("Hello World".contains!compare("HELLO"));
+    assert(!"Hello World".contains!compare("Nope"));
+}
+
 public:
 
 
@@ -20,7 +56,7 @@ auto contains(alias pred, Index = DefaultFindIndex, Iter)(
 
 auto contains(alias pred, Index = DefaultFindIndex, Iter, Find)(
     Iter iter, Find subject
-) if(canFindElementEager!(pred, Index, Iter, true)){
+) if(canFindIterable!(pred, Index, Iter, Find, true)){
     return containsiter!(pred, Index, Iter, Find)(iter, subject);
 }
 
@@ -68,18 +104,31 @@ version(unittest){
 unittest{
     tests("Contains", {
         tests("Element", {
-            test("hello".contains('h'));
-            test("hello".contains('e'));
-            test("hello".contains('l'));
-            test("hello".contains('o'));
-            testf("hello".contains('z'));
+            tests("Implicit predicate", {
+                test("hello".contains('h'));
+                test("hello".contains('e'));
+                test("hello".contains('l'));
+                test("hello".contains('o'));
+                testf("hello".contains('z'));
+            });
+            tests("Explicit predicate", {
+                test("hello".contains!(ch => ch == 'l'));
+                testf("hello".contains!(ch => ch == 'x'));
+            });
         });
         tests("Iterable", {
-            test("hello world".contains("hello"));
-            test("hello world".contains("world"));
-            test("hello world".contains("hello world"));
-            testf("hello world".contains(""));
-            testf("hello world".contains("yo"));
+            tests("Implicit predicate", {
+                test("hello world".contains("hello"));
+                test("hello world".contains("world"));
+                test("hello world".contains("hello world"));
+                testf("hello world".contains(""));
+                testf("hello world".contains("yo"));
+            });
+            tests("Explicit predicate", {
+                alias comp = (a, b) => (a == b - 1);
+                test([0, 1, 2].contains!comp([2, 3]));
+                testf([0, 1, 2].contains!comp([5]));
+            });
         });
     });
 }
