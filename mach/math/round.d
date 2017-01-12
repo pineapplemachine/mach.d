ff277a286ce776e3a6d97b6596dc55228f861f4c
Sophie Kirschner <pineapplemachine@gmail.com>
2017-01-12 16:19:11 -0500

    Updated mach.math.round to include floor and ceil functions
    Also subjected it to a general facelift: cleaned up code and tests and added docs

--- mach/math/round.d
+++ mach/math/round.d
@@ -2,30 +2,71 @@ module mach.math.round;
 
 private:
 
-import mach.traits : isNumeric, isIntegral;
-import std.math : abs;
+import mach.traits : isNumeric, isIntegral, isFloatingPoint;
+import mach.math.abs : abs;
+import mach.math.floats : ffloor, fceil, fisinf, fisnan;
+
+/++ Docs
+
+The `round`, `floor`, and `ceil` functions can be used to round a value to
+near integers.
+The `round` function rounds to the nearest integer, and rounds up when the
+fractional part exactly equals 0.5.
+The `floor` function rounds down to the nearest integer,
+and the `ceil` function rounds up to the nearest integer.
+
+Though these functions are implemented for all numeric primitives, note that
+when their inputs are integers they will always return the input itself.
+Actual computation really only happens when the input is a floating point value.
+
++/
+
+unittest{ /// Example
+    assert(floor(100) == 100);
+    assert(floor(200.5) == 200);
+    assert(floor(-200.5) == -201);
+}
+
+unittest{ /// Example
+    assert(ceil(100) == 100);
+    assert(ceil(200.5) == 201);
+    assert(ceil(-200.5) == -200);
+}
+
+unittest{ /// Example
+    assert(round(100) == 100);
+    assert(round(200.25) == 200);
+    assert(round(200.75) == 201);
+    assert(round(-200.25) == -200);
+    assert(round(-200.75) == -201);
+}
 
 public:
 
 
 
 /// Round to the nearest whole number.
-R round(R = int, N)(in N number) if(isNumeric!N && isNumeric!R){
-    static if(isIntegral!N){
-        return cast(R) number;
+/// TODO: Write a more efficient implementation and put it in mach.math.floats
+T round(T)(in T value) if(isNumeric!T){
+    static if(isIntegral!T){
+        return value;
     }else{
-        auto remainder = number % 1;
-        auto uremainder = abs(remainder);
-        auto add = (uremainder >= 0.5) ? (number > 0 ? 1 : -1) : 0;
-        static if(isIntegral!R){
-            return cast(R) (number + add);
+        if(value.fisnan || value.fisinf){
+            return value;
         }else{
-            return cast(R) (number + add - remainder);
+            auto remainder = value % 1;
+            auto uremainder = value > 0 ? remainder : 1 + remainder;
+            if(uremainder >= 0.5){
+                return value.fceil;
+            }else{
+                return value.ffloor;
+            }
         }
     }
 }
 
 /// Get the ceiling of one number divided by another.
+/// TODO: There's probably a better place for this functionality than here.
 R divceil(R = int, N)(in N x, in N y) if(isNumeric!N && isNumeric!R){
     static if(isIntegral!N){
         auto floor = x / y;
@@ -44,51 +85,84 @@ R divceil(R = int, N)(in N x, in N y) if(isNumeric!N && isNumeric!R){
 
 
 
-version(unittest){
-    private:
-    import mach.test;
+/// Get a value, rounded down to the nearest integer.
+T floor(T)(in T value) if(isIntegral!T){
+    return value;
 }
+/// ditto
+T floor(T)(in T value) if(isFloatingPoint!T){
+    return ffloor(value);
+}
+
+/// Get a value, rounded up to the nearest integer.
+T ceil(T)(in T value) if(isIntegral!T){
+    return value;
+}
+/// Get a value, rounded up to the nearest integer.
+T ceil(T)(in T value) if(isFloatingPoint!T){
+    return fceil(value);
+}
+
+
+
+private version(unittest){
+    import mach.meta : Aliases;
+}
+
 unittest{
-    tests("Rounding", {
-        tests("Integers", {
-            testeq(round(0), 0);
-            testeq(round(1), 1);
-            testeq(round(-1), -1);
-        });
-        tests("Reals", {
-            testeq(round(0.0), 0);
-            testeq(round(0.25), 0);
-            testeq(round(0.5), 1);
-            testeq(round(0.75), 1);
-            testeq(round(1.0), 1);
-            testeq(round(1.25), 1);
-            testeq(round(1.5), 2);
-            testeq(round(-0.25), 0);
-            testeq(round(-0.5), -1);
-            testeq(round(-0.75), -1);
-        });
-        tests("Returning reals", {
-            testeq(round!real(0.0), 0.0);
-            testeq(round!real(0.5), 1.0);
-            testeq(round!real(-0.5), -1.0);
-        });
-    });
-    tests("Ceil of division", {
-        tests("Integers", {
-            testeq(divceil(10, 2), 5);
-            testeq(divceil(10, 3), 4);
-            testeq(divceil(-10, 3), -3);
-            testeq(divceil!real(10, 2), 5.0);
-            testeq(divceil!real(10, 3), 4.0);
-            testeq(divceil!real(-10, 3), -3.0);
-        });
-        tests("Reals", {
-            testeq(divceil(10.0, 2.0), 5);
-            testeq(divceil(10.0, 3.0), 4);
-            testeq(divceil(-10.0, 3.0), -3);
-            testeq(divceil!real(10.0, 2.0), 5.0);
-            testeq(divceil!real(10.0, 3.0), 4.0);
-            testeq(divceil!real(-10.0, 3.0), -3.0);
-        });
-    });
-}    
+    // Round integers
+    assert(round(0) == 0);
+    assert(round(1) == 1);
+    assert(round(-1) == -1);
+    // Round floats
+    assert(round(0.0) == 0);
+    assert(round(0.25) == 0);
+    assert(round(0.5) == 1);
+    assert(round(0.75) == 1);
+    assert(round(1.0) == 1);
+    assert(round(1.25) == 1);
+    assert(round(1.5) == 2);
+    assert(round(-0.25) == 0);
+    assert(round(-0.5) == 0);
+    assert(round(-0.75) == -1);
+}
+
+unittest{
+    // Integers
+    assert(divceil(10, 2) == 5);
+    assert(divceil(10, 3) == 4);
+    assert(divceil(-10, 3) == -3);
+    assert(divceil!real(10, 2) == 5.0);
+    assert(divceil!real(10, 3) == 4.0);
+    assert(divceil!real(-10, 3) == -3.0);
+    // Floats
+    assert(divceil(10.0, 2.0) == 5);
+    assert(divceil(10.0, 3.0) == 4);
+    assert(divceil(-10.0, 3.0) == -3);
+    assert(divceil!real(10.0, 2.0) == 5.0);
+    assert(divceil!real(10.0, 3.0) == 4.0);
+    assert(divceil!real(-10.0, 3.0) == -3.0);
+}
+
+unittest{
+    foreach(T; Aliases!(ubyte, byte, uint, int, ulong, long, float, double, real)){
+        assert(T(0).floor == 0);
+        assert(T(1).floor == 1);
+        assert(T(100).floor == 100);
+        assert(T(0).ceil == 0);
+        assert(T(1).ceil == 1);
+        assert(T(100).ceil == 100);
+    }
+    foreach(T; Aliases!(byte, int, long, float, double, real)){
+        assert(T(-1).floor == -1);
+        assert(T(-100).floor == -100);
+        assert(T(-1).ceil == -1);
+        assert(T(-100).ceil == -100);
+    }
+    foreach(T; Aliases!(float, double, real)){
+        assert(T(0.5).floor == 0);
+        assert(T(-0.5).floor == -1);
+        assert(T(0.5).ceil == 1);
+        assert(T(-0.5).ceil == 0);
+    }
+}
