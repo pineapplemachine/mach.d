671319fdc4ea9833031fa6384776ac3aad49734c
Sophie Kirschner <pineapplemachine@gmail.com>
2019-09-25 16:18:44 +0300

    Improve mach.meta.repeat template implementation
    this new implementation should result in far fewer template instantiations

--- mach/meta/repeat.d
+++ mach/meta/repeat.d
@@ -25,23 +25,53 @@ public:
 
 
 /// Repeat a list of aliases some given number of times.
-template Repeat(size_t count, T...){
-    static if(count == 0 || T.length == 0){
+template Repeat(size_t count, T...) {
+    static if(count == 0 || T.length == 0) {
         alias Repeat = Aliases!();
-    }else static if(count == 1){
+    }
+    else static if(count == 1) {
         alias Repeat = T;
-    }else{
-        alias Repeat = Aliases!(T, Repeat!(count - 1, T));
+    }
+    else static if(count == 2) {
+        alias Repeat = Aliases!(T, T);
+    }
+    else static if(count == 3) {
+        alias Repeat = Aliases!(T, T, T);
+    }
+    else static if(count == 4) {
+        alias Repeat = Aliases!(T, T, T, T);
+    }
+    else {
+        alias left = Aliases!(Repeat!(count / 2, T));
+        alias right = Aliases!(Repeat!(count - (count / 2), T));
+        alias Repeat = Aliases!(left, right);
     }
 }
 
 
 
-unittest{
+unittest { /// Repeat an empty sequence
     static assert(is(Repeat!(0) == Aliases!()));
     static assert(is(Repeat!(1) == Aliases!()));
+}
+
+unittest { /// Repeat a single item
+    static assert(is(Repeat!(0, int) == Aliases!()));
+    static assert(is(Repeat!(1, int) == Aliases!(int)));
+    static assert(is(Repeat!(2, int) == Aliases!(int, int)));
+    static assert(is(Repeat!(3, int) == Aliases!(int, int, int)));
+    static assert(is(Repeat!(4, int) == Aliases!(int, int, int, int)));
+    static assert(is(Repeat!(5, int) == Aliases!(int, int, int, int, int)));
+    static assert(is(Repeat!(6, int) == Aliases!(int, int, int, int, int, int)));
+}
+
+unittest { /// Repeat a sequence containing multiple items
     static assert(is(Repeat!(0, int, long) == Aliases!()));
     static assert(is(Repeat!(1, int, long) == Aliases!(int, long)));
     static assert(is(Repeat!(2, int, long) == Aliases!(int, long, int, long)));
     static assert(is(Repeat!(3, int, long) == Aliases!(int, long, int, long, int, long)));
+    static assert(is(Repeat!(6, int, long) == Aliases!(
+        int, long, int, long, int, long,
+        int, long, int, long, int, long
+    )));
 }
